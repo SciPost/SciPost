c8f51845323d508182d99bfa73a7f3cdde36888e
J.-S. Caux <J.S.Caux@uva.nl>
2016-03-17 07:53:11 +0100

    Last version before ratings refactoring

--- ratings/urls.py
+++ ratings/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import include, url
-from django.views.generic import TemplateVie
+from django.views.generic import TemplateView
 
 from . import views
 
--- theses/urls.py
+++ theses/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import include, url
+from django.views.generic import TemplateView
 
 from . import views
 
