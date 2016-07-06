917ab280d4fbb041b464d3b95e4444259869afa0
J.-S. Caux <J.S.Caux@uva.nl>
2016-07-06 12:10:27 +0200

    Correct bug in submission/views.py: Group import missing

--- submissions/views.py
+++ submissions/views.py
@@ -7,7 +7,7 @@ from django.utils import timezone
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required, permission_required
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group, Permission
 from django.core.mail import EmailMessage
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
