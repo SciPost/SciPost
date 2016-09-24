6a65b04fe3d445b8ceaea7b3a30da36c50ae5250
J.-S. Caux <J.S.Caux@uva.nl>
2016-09-24 06:58:52 +0200

    Enable auto documentation in admin

--- SciPost_v1/settings.py
+++ SciPost_v1/settings.py
@@ -58,6 +58,7 @@ os.environ['wsgi.url_scheme'] = 'https'
 
 INSTALLED_APPS = (
     'django.contrib.admin',
+    'django.contrib.admindocs',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
--- SciPost_v1/urls.py
+++ SciPost_v1/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('scipost.urls', namespace="scipost")),
     url(r'^contributor/', include('scipost.urls', namespace="scipost")),
