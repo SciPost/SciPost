b1dac5d75dd2659dd16283624db16a51bf62b701
Geert Kapteijns <ghkapteijns@gmail.com>
2016-12-15 14:38:33 +0100

    Change .gitignore to include **/fixtures/*.json
    Create fixtures for contributors, groups and permissions.

---
+++ scipost/factories.py
@@ -0,0 +1,13 @@
+import factory
+
+from django.contrib.auth import get_user_model
+
+from .models import Contributor
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = get_user_model()
+
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
