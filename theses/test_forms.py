67ac2c59f5abb9c6e180b9bf3587bcea1fae98b7
Geert Kapteijns <ghkapteijns@gmail.com>
2016-12-16 22:32:12 +0100

    Extract model_form_data to common/helpers
    This function helps to create form data from models.
    See example given.

--- theses/test_forms.py
+++ theses/test_forms.py
@@ -5,27 +5,23 @@ from django.test import TestCase
 from .factories import ThesisLinkFactory
 from .forms import RequestThesisLinkForm
 
-
-def valid_form_data(thesis_link):
-    thesis_link_data = thesis_link.__dict__
-    form_fields = list(RequestThesisLinkForm().fields.keys())
-    return filter_keys(thesis_link_data, form_fields)
-
-
-def filter_keys(dictionary, keys_to_keep):
-    return {key: dictionary[key] for key in keys_to_keep}
+from common.helpers import model_form_data
 
 
 class TestRequestThesisLink(TestCase):
     fixtures = ['permissions', 'groups']
 
+    def setUp(self):
+        self.valid_form_data = model_form_data(ThesisLinkFactory(), RequestThesisLinkForm)
+
     def test_valid_data_is_valid(self):
-        thesis_data = valid_form_data(ThesisLinkFactory())
-        form = RequestThesisLinkForm(thesis_data)
+        form_data = self.valid_form_data
+        form = RequestThesisLinkForm(self.valid_form_data)
         self.assertTrue(form.is_valid())
 
     def test_empty_domain_is_invalid(self):
-        thesis_data = valid_form_data(ThesisLinkFactory(domain=''))
-        form = RequestThesisLinkForm(thesis_data)
+        form_data = self.valid_form_data
+        form_data['domain'] = ''
+        form = RequestThesisLinkForm(form_data)
         form.is_valid()
         self.assertEqual(form.errors['domain'], ['This field is required.'])
