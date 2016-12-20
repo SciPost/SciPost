f97fde0edd07c13e76114b592f3fe423ec3fba44
Geert Kapteijns <ghkapteijns@gmail.com>
2016-12-20 22:33:13 +0100

    Set default strategy for FormFactory to build

--- common/helpers/factories.py
+++ common/helpers/factories.py
@@ -2,6 +2,9 @@ import factory
 
 
 class FormFactory(factory.Factory):
+    class Meta:
+        strategy = factory.BUILD_STRATEGY
+
     @classmethod
     def _build(cls, model_class, *args, **kwargs):
         return model_class(kwargs)
