ba3de1f87c4c6d79fd27aa4fc70c85130d54d0de
Jorran de Wit <jorrandewit@outlook.com>
2017-01-31 14:29:00 +0100

    Add missing migrations
    Migrations involve appending default values to model fields

--- commentaries/models.py
+++ commentaries/models.py
@@ -52,7 +52,7 @@ class Commentary(TimeStampedModel):
         blank=True)
     metadata = JSONField(default={}, blank=True, null=True)
     arxiv_or_DOI_string = models.CharField(
-        max_length=100,
+        max_length=100, default='',
         verbose_name='string form of arxiv nr or DOI for commentary url')
     author_list = models.CharField(max_length=1000)
 
