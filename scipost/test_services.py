f34de135148c73c556e734a86e39eef7d0280f1d
Jorran de Wit <jorrandewit@outlook.com>
2017-02-06 07:30:27 +0100

    Use service tests with Submission

--- scipost/test_services.py
+++ scipost/test_services.py
@@ -1,11 +1,14 @@
 from django.test import TestCase
+
 from .services import ArxivCaller
+
+from submissions.models import Submission
 
 
 class ArxivCallerTest(TestCase):
 
     def test_correct_lookup(self):
-        caller = ArxivCaller()
+        caller = ArxivCaller(Submission)
 
         caller.process('1611.09574v1')
 
@@ -13,21 +16,21 @@ class ArxivCallerTest(TestCase):
         self.assertIn('entries', caller.metadata)
 
     def test_errorcode_for_non_existing_paper(self):
-        caller = ArxivCaller()
+        caller = ArxivCaller(Submission)
 
         caller.process('2611.09574v1')
         self.assertEqual(caller.is_valid(), False)
         self.assertEqual(caller.errorcode, 'preprint_does_not_exist')
 
     def test_errorcode_for_bad_request(self):
-        caller = ArxivCaller()
+        caller = ArxivCaller(Submission)
 
         caller.process('161109574v1')
         self.assertEqual(caller.is_valid(), False)
         self.assertEqual(caller.errorcode, 'arxiv_bad_request')
 
     def test_errorcode_for_already_published_journal_ref(self):
-        caller = ArxivCaller()
+        caller = ArxivCaller(Submission)
 
         caller.process('1412.0006v1')
         self.assertEqual(caller.is_valid(), False)
@@ -36,7 +39,7 @@ class ArxivCallerTest(TestCase):
 
     def test_errorcode_no_version_nr(self):
         # Should be already caught in form validation
-        caller = ArxivCaller()
+        caller = ArxivCaller(Submission)
 
         caller.process('1412.0006')
         self.assertEqual(caller.is_valid(), False)
