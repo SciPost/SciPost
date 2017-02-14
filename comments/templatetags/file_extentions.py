248c8d30fc5ec8ce9cb0f3877372480a9bc79e04
Jorran de Wit <jorrandewit@outlook.com>
2017-02-14 01:58:35 +0100

    Prepare for single template comment form
    Edit styles to make old comment form better readable.
     This commit includes a new template which should eventualy act
     as a replacement for crispyforms which do not allow for
     easy bootstrap implementation.

---
+++ comments/templatetags/file_extentions.py
@@ -0,0 +1,17 @@
+from django import template
+
+from ..constants import EXTENTIONS_IMAGES, EXTENTIONS_PDF
+from ..utils import validate_file_extention
+
+
+register = template.Library()
+
+
+@register.filter
+def is_image(value):
+    return validate_file_extention(value, EXTENTIONS_IMAGES)
+
+
+@register.filter
+def is_pdf(value):
+    return validate_file_extention(value, EXTENTIONS_PDF)
