7406deae097f5b93576b6497e7ba82a53599ae34
Geert Kapteijns <ghkapteijns@gmail.com>
2017-02-15 18:09:34 +0100

    Change theses search form method to GET. Extract query logic to custom manager.

---
+++ theses/managers.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class ThesisLinkManager(models.Manager):
+    def search_results(self, form):
+        return self.vetted().filter(
+            title__icontains=form.cleaned_data['title_keyword'],
+            author__icontains=form.cleaned_data['author'],
+            abstract__icontains=form.cleaned_data['abstract_keyword'],
+            supervisor__icontains=form.cleaned_data['supervisor'],
+        ).order_by('-defense_date')
+
+    def latest(self, n):
+        return self.vetted().order_by('latest_activity')[:n]
+
+    def vetted(self):
+        return self.filter(vetted=True)
