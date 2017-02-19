c5c63603fbc65bd7f8ef513e96c2910d02ecc645
Geert Kapteijns <ghkapteijns@gmail.com>
2017-02-19 10:40:20 +0100

    Add additional fields SubmissionFactory

--- submissions/factories.py
+++ submissions/factories.py
@@ -1,4 +1,6 @@
 import factory
+import random
+import string
 
 from scipost.factories import ContributorFactory
 
@@ -9,17 +11,28 @@ class SubmissionFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Submission
 
+    author_list = factory.Faker('name')
     submitted_by = factory.SubFactory(ContributorFactory)
     submitted_to_journal = 'SciPost Physics'
     title = factory.Faker('bs')
     abstract = factory.Faker('text')
     arxiv_link = factory.Faker('uri')
+    arxiv_identifier_w_vn_nr = factory.Sequence(lambda n: random_arxiv_identifier_with_version_number())
+    arxiv_identifier_wo_vn_nr = factory.LazyAttribute(lambda obj: obj.arxiv_identifier_w_vn_nr[0:-2])
+    domain = 'E'
 
 
-    @factory.post_generation
-    def authors(self, create, extracted, **kwargs):
-        # Add a single author if factory is invoked with strategy 'create'
-        if not create:
-            return
-        else:
-            self.authors.add(ContributorFactory())
+class EICassignedSubmissionFactory(SubmissionFactory):
+    status = 'EICassigned'
+    editor_in_charge = factory.SubFactory(ContributorFactory)
+
+
+
+def random_arxiv_identifier_with_version_number():
+    return random_arxiv_identifier_without_version_number() + "v0"
+
+def random_arxiv_identifier_without_version_number():
+    return random_digits(4) + "." + random_digits(5)
+
+def random_digits(n):
+    return "".join(random.choice(string.digits) for _ in range(n))
