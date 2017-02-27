f2c1eaf5458558dbfd8ab1b16929ceb4d4275691
Jorran de Wit <jorrandewit@outlook.com>
2017-02-27 22:50:15 +0100

    Add staging_release settings

---
+++ SciPost_v1/settings/staging_release.py
@@ -0,0 +1,26 @@
+from .base import *
+
+# This file is meant for the server used for the release branches
+#
+
+# THE MAIN THING HERE
+DEBUG = False
+
+# Static and media
+STATIC_URL = '/static/'
+STATIC_ROOT = '/home/jdewit/webapps/scipost_static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/home/jdewit/webapps/scipost_media/'
+WEBPACK_LOADER = {
+    'DEFAULT': {
+        'CACHE': True,
+        'BUNDLE_DIR_NAME': '/home/jdewit/webapps/scipost_static/bundles/',
+    }
+}
+
+# Error reporting
+ADMINS = MANAGERS = (('J. de Wit', 'jorrandewit@outlook.com'), )
+
+# Cookies
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
