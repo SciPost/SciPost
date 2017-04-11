7c4765efa43b2945a6af07c85f7570442bce36ee
Jorran de Wit <jorrandewit@outlook.com>
2017-04-11 21:23:03 +0200

    Minor improvements to about page
    Use toggling text on specialization block;
    Use card-deck to get proper alphabetic ordering;
    Add supporting text to spec. block.

--- scipost/static/scipost/about.js
+++ scipost/static/scipost/about.js
@@ -5,6 +5,9 @@ $(function() {
         var el = $($(this).attr('data-target'));
         el.toggle();
 
+        // Switch texts of link
+        $('[href="' + $(this).attr('href') + '"]').toggle();
+
         // Reset active search after closing the box
         if(!el.is(':visible')) {
             $('.all-specializations .specialization')
@@ -33,7 +36,7 @@ $(function() {
     .on('search-specialization', function() {
         // Reset: searching multiple specializations is not supported
         $('.search-contributors.active-search').removeClass('active-search');
-        $('.contributor-col.active').removeClass('active');
+        $('.contributor.active').removeClass('active');
         $('.specialization.active-search').not(this).removeClass('active-search');
 
         var el = $(this);
@@ -44,7 +47,7 @@ $(function() {
             // Add class to specialized Contributors
             var code = el.attr('data-specialization');
             $('.single[data-specialization="' + code + '"]')
-            .parents('.contributor-col')
+            .parents('.contributor')
             .addClass('active');
         }
     });
