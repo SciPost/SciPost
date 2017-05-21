d3a46369bf2685655e10cf9d7f7446c2d782f478
Jorran de Wit <jorrandewit@outlook.com>
2017-05-21 16:51:35 +0200

    Hide invite referee only if not found after query

--- submissions/views.py
+++ submissions/views.py
@@ -569,34 +569,35 @@ def cycle_form_submit(request, arxiv_identifier_w_vn_nr):
 def select_referee(request, arxiv_identifier_w_vn_nr):
     submission = get_object_or_404(Submission.objects.get_pool(request.user),
                                    arxiv_identifier_w_vn_nr=arxiv_identifier_w_vn_nr)
+    context = {}
     queryresults = ''
-    if request.method == 'POST':
-        ref_search_form = RefereeSelectForm(request.POST)
-        if ref_search_form.is_valid():
-            contributors_found = Contributor.objects.filter(
-                user__last_name__icontains=ref_search_form.cleaned_data['last_name'])
-            # Check for recent co-authorship (thus referee disqualification)
-            if submission.metadata is not None:
-                sub_auth_boolean_str = '((' + (submission
-                                               .metadata['entries'][0]['authors'][0]['name']
-                                               .split()[-1])
-                for author in submission.metadata['entries'][0]['authors'][1:]:
-                    sub_auth_boolean_str += '+OR+' + author['name'].split()[-1]
-                sub_auth_boolean_str += ')+AND+'
-                search_str = sub_auth_boolean_str + ref_search_form.cleaned_data['last_name'] + ')'
-                queryurl = ('http://export.arxiv.org/api/query?search_query=au:%s'
-                            % search_str + '&sortBy=submittedDate&sortOrder=descending'
-                            '&max_results=5')
-                arxivquery = feedparser.parse(queryurl)
-                queryresults = arxivquery
-    else:
-        ref_search_form = RefereeSelectForm()
-        contributors_found = None
-    ref_recruit_form = RefereeRecruitmentForm()
-    context = {'submission': submission, 'ref_search_form': ref_search_form,
-               'contributors_found': contributors_found,
-               'ref_recruit_form': ref_recruit_form,
-               'queryresults': queryresults}
+
+    ref_search_form = RefereeSelectForm(request.POST or None)
+    if ref_search_form.is_valid():
+        contributors_found = Contributor.objects.filter(
+            user__last_name__icontains=ref_search_form.cleaned_data['last_name'])
+        context['contributors_found'] = contributors_found
+        # Check for recent co-authorship (thus referee disqualification)
+        if submission.metadata is not None:
+            sub_auth_boolean_str = '((' + (submission
+                                           .metadata['entries'][0]['authors'][0]['name']
+                                           .split()[-1])
+            for author in submission.metadata['entries'][0]['authors'][1:]:
+                sub_auth_boolean_str += '+OR+' + author['name'].split()[-1]
+            sub_auth_boolean_str += ')+AND+'
+            search_str = sub_auth_boolean_str + ref_search_form.cleaned_data['last_name'] + ')'
+            queryurl = ('http://export.arxiv.org/api/query?search_query=au:%s'
+                        % search_str + '&sortBy=submittedDate&sortOrder=descending'
+                        '&max_results=5')
+            arxivquery = feedparser.parse(queryurl)
+            queryresults = arxivquery
+        context['ref_recruit_form'] = RefereeRecruitmentForm()
+
+    context.update({
+        'submission': submission,
+        'ref_search_form': ref_search_form,
+        'queryresults': queryresults
+    })
     return render(request, 'submissions/select_referee.html', context)
 
 
