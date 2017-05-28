8adf04eb23351267720be322a6cfb47240fbebbe
J.-S. Caux <J.S.Caux@uva.nl>
2017-05-28 18:54:45 +0200

    Improve admin (order objects in select forms etc)

--- virtualmeetings/admin.py
+++ virtualmeetings/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
+from django import forms
+
 from .models import VGM, Feedback, Nomination, Motion
+
+from scipost.models import Contributor
 
 
 class VGMAdmin(admin.ModelAdmin):
@@ -17,15 +21,51 @@ class FeedbackAdmin(admin.ModelAdmin):
 admin.site.register(Feedback, FeedbackAdmin)
 
 
+class NominationAdminForm(forms.ModelForm):
+    in_agreement = forms.ModelMultipleChoiceField(
+        queryset=Contributor.objects.filter(
+            user__groups__name__in=['Editorial College'],
+        ).order_by('user__last_name'))
+    in_notsure = forms.ModelMultipleChoiceField(
+        queryset=Contributor.objects.filter(
+            user__groups__name__in=['Editorial College'],
+        ).order_by('user__last_name'))
+    in_disagreement = forms.ModelMultipleChoiceField(
+        queryset=Contributor.objects.filter(
+            user__groups__name__in=['Editorial College'],
+        ).order_by('user__last_name'))
+
+    class Meta:
+        model = Nomination
+        fields = '__all__'
+
 class NominationAdmin(admin.ModelAdmin):
     search_fields = ['last_name', 'first_name', 'by']
-
+    form = NominationAdminForm
 
 admin.site.register(Nomination, NominationAdmin)
 
 
+class MotionAdminForm(forms.ModelForm):
+    in_agreement = forms.ModelMultipleChoiceField(
+        queryset=Contributor.objects.filter(
+            user__groups__name__in=['Editorial College'],
+        ).order_by('user__last_name'))
+    in_notsure = forms.ModelMultipleChoiceField(
+        queryset=Contributor.objects.filter(
+            user__groups__name__in=['Editorial College'],
+        ).order_by('user__last_name'))
+    in_disagreement = forms.ModelMultipleChoiceField(
+        queryset=Contributor.objects.filter(
+            user__groups__name__in=['Editorial College'],
+        ).order_by('user__last_name'))
+
+    class Meta:
+        model = Motion
+        fields = '__all__'
+
 class MotionAdmin(admin.ModelAdmin):
     search_fields = ['background', 'motion', 'put_forward_by']
-
+    form = MotionAdminForm
 
 admin.site.register(Motion, MotionAdmin)
