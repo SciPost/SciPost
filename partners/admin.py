bf9275e3b00f1def2e8f8b358c34bda9720b34ae
Jorran de Wit <jorrandewit@outlook.com>
2017-06-04 22:36:40 +0200

    Quick admin improvements

--- partners/admin.py
+++ partners/admin.py
@@ -1,17 +1,26 @@
 from django.contrib import admin
 
-from .models import Contact, Partner, Consortium,\
-    ProspectivePartner, MembershipAgreement
+from .models import Contact, Partner, Consortium, ProspectivePartner, MembershipAgreement,\
+                    ProspectiveContact
 
 
-admin.site.register(Contact)
+class ProspectiveContactInline(admin.TabularInline):
+    model = ProspectiveContact
+    extra = 0
 
 
 class PartnerAdmin(admin.ModelAdmin):
-    search_fields = ['institution', ]
+    search_fields = ('institution', )
+
+
+class ProspectivePartnerAdmin(admin.ModelAdmin):
+    inlines = (ProspectiveContactInline,)
+    list_display = ('institution_name', 'date_received', 'status')
+    list_filter = ('kind', 'status')
 
 
 admin.site.register(Partner, PartnerAdmin)
+admin.site.register(Contact)
 admin.site.register(Consortium)
-admin.site.register(ProspectivePartner)
+admin.site.register(ProspectivePartner, ProspectivePartnerAdmin)
 admin.site.register(MembershipAgreement)
