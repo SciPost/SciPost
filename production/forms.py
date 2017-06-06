de48551357ef79f5a0e3dd7d27bb5bc7e165fb25
Jorran de Wit <jorrandewit@outlook.com>
2017-06-06 17:27:36 +0200

    General improvements
    Include the removal of redundant HTML, fix security fix (view permission) and modelform use.

--- production/forms.py
+++ production/forms.py
@@ -6,8 +6,13 @@ from .models import ProductionEvent
 class ProductionEventForm(forms.ModelForm):
     class Meta:
         model = ProductionEvent
-        exclude = ['stream', 'noted_on', 'noted_by']
+        fields = (
+            'event',
+            'comments',
+            'duration'
+        )
         widgets = {
+            'stream': forms.HiddenInput(),
             'comments': forms.Textarea(attrs={'rows': 4}),
             'duration': forms.TextInput(attrs={'placeholder': 'HH:MM:SS'})
         }
