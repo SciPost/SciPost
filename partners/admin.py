749d6ad5bcc3bb8469e45a1ffc8e2dcde610ed32
Jorran de Wit <jorrandewit@outlook.com>
2017-06-20 16:21:31 +0200

    Add Contact to admin as inline

--- partners/admin.py
+++ partners/admin.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
 
-from .models import Contact, Partner, PartnerEvent, Consortium,\
+from .models import Contact, Partner, Consortium,\
                     ProspectivePartner, ProspectiveContact, ProspectivePartnerEvent,\
                     MembershipAgreement
+
 
 class ProspectiveContactInline(admin.TabularInline):
     model = ProspectiveContact
     extra = 0
 
+
 class ProspectivePartnerEventInline(admin.TabularInline):
     model = ProspectivePartnerEvent
+
 
 class ProspectivePartnerAdmin(admin.ModelAdmin):
     inlines = (ProspectiveContactInline, ProspectivePartnerEventInline,)
@@ -21,8 +24,14 @@ class PartnerAdmin(admin.ModelAdmin):
     search_fields = ('institution', )
 
 
+class ContactInline(admin.StackedInline):
+    model = Contact
+    extra = 0
+    min_num = 0
+    verbose_name = 'Contact (Partners)'
+
+
 admin.site.register(Partner, PartnerAdmin)
-admin.site.register(Contact)
 admin.site.register(Consortium)
 admin.site.register(ProspectivePartner, ProspectivePartnerAdmin)
 admin.site.register(MembershipAgreement)
