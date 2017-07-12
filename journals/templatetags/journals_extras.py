b73ed0439097d72e5534fb2b8926b7fac09895da
J.-S. Caux <J.S.Caux@uva.nl>
2017-07-12 08:46:57 +0200

    Improve manage metadata

--- journals/templatetags/journals_extras.py
+++ journals/templatetags/journals_extras.py
@@ -10,8 +10,17 @@ def paper_nr_string_filter(nr):
     return paper_nr_string(nr)
 
 @register.filter(name='latest_successful_crossref_deposit')
-def latest_crossref_deposition_date(publication):
+def latest_successful_crossref_deposit(publication):
     latest = publication.deposit_set.filter(
+        deposit_successful=True).order_by('-deposition_date').first()
+    if latest:
+        return latest.deposition_date.strftime('%Y-%m-%d')
+    else:
+        return "No successful deposit found"
+
+@register.filter(name='latest_successful_DOAJ_deposit')
+def latest_successful_DOAJ_deposit(publication):
+    latest = publication.doajdeposit_set.filter(
         deposit_successful=True).order_by('-deposition_date').first()
     if latest:
         return latest.deposition_date.strftime('%Y-%m-%d')
