6297b20c0683808ad0ab078b2685d421225a1a3d
J.-S. Caux <J.S.Caux@uva.nl>
2017-10-08 20:30:31 +0200

    Add ReCaptcha to petition sign form

--- petitions/forms.py
+++ petitions/forms.py
@@ -1,9 +1,13 @@
 from django import forms
 
+from captcha.fields import ReCaptchaField
+
 from .models import PetitionSignatory
 
 
 class SignPetitionForm(forms.ModelForm):
+    captcha = ReCaptchaField(attrs={'theme': 'clean'}, label='*Please verify to continue:')
+
     class Meta:
         model = PetitionSignatory
         fields = ['title', 'first_name', 'last_name',
