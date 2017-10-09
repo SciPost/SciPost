69f0f49cdced8fb5868afb5666a02007d30222f3
J.-S. Caux <J.S.Caux@uva.nl>
2017-10-09 09:19:11 +0200

    Remove unneeded text

--- petitions/utils.py
+++ petitions/utils.py
@@ -1,20 +1,6 @@
 from common.utils import BaseMailUtil
 
 
-SPB_PETITION_THANKS = (
-    'Many thanks for signing the petition!\n\n'
-    'If you have not done so already, you can really further help SciPost convince '
-    'your institution, library and/or funding agency to become Supporting Partners '
-    'by sending a personalized email to one of their representatives; we have a '
-    'handy email template for you to use at https://scipost.org/partners/.\n\n'
-    'You can also point them to our prospectus and draft agreement on that same '
-    'page.\n\nWe are very grateful for your help.\n\nThe SciPost Team'
-    )
-
-SPB_PETITION_THANKS_HTML = (
-)
-
-
 class PetitionUtils(BaseMailUtil):
     mail_sender = 'petitions@scipost.org'
     mail_sender_title = 'SciPost petitions'
