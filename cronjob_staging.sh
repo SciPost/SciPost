e9521cf5f3ca555d974ba405279f6c86cc8b5ea6
Jorran de Wit <jorrandewit@outlook.com>
2017-10-21 18:35:38 +0200

    Run Cronjobs for Referees stging

---
+++ cronjob_staging.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /home/jdewit/webapps/scipost/SciPost_v1/
+export DJANGO_SETTINGS_MODULE='SciPost_v1.settings.staging_release'
+
+python3.5 manage.py remind_fellows_to_submit_report
