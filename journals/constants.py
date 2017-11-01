e80dc6e6f2ca01bef74c79b80a8bdd6e53a552e7
Jorran de Wit <jorrandewit@outlook.com>
2017-11-01 09:34:28 +0100

    Fix incomplete journal name in submit manuscript form

--- journals/constants.py
+++ journals/constants.py
@@ -8,7 +8,7 @@ SCIPOST_JOURNAL_PHYSICS_PROC = 'SciPostPhysProc'
 SCIPOST_JOURNALS_SUBMIT = (
     (SCIPOST_JOURNAL_PHYSICS, 'SciPost Physics'),
     (SCIPOST_JOURNAL_PHYSICS_LECTURE_NOTES, 'SciPost Physics Lecture Notes'),
-    (SCIPOST_JOURNAL_PHYSICS_PROC, 'SciPost Proceedings')
+    (SCIPOST_JOURNAL_PHYSICS_PROC, 'SciPost Physics Proceedings')
 )
 
 # Journal closed for submission
