2ed7fdaad112ac1bd8e8293afe2302f32106374e
Jorran de Wit <jorrandewit@outlook.com>
2017-11-10 10:39:49 +0100

    Update texts

--- affiliations/forms.py
+++ affiliations/forms.py
@@ -14,7 +14,7 @@ from .models import Affiliation, Institution
 class AffiliationForm(forms.ModelForm):
     name = forms.CharField(label='* Institution', max_length=300)
     country = LazyTypedChoiceField(
-        choices=countries, label='* Country', widget=CountrySelectWidget())
+        choices=countries, label='* Country', widget=CountrySelectWidget(), initial='NL')
 
     class Meta:
         model = Affiliation
