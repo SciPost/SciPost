8cb4ba3e027ed53cedf142304fd41a175036da85
Jorran de Wit <jorrandewit@outlook.com>
2017-11-22 20:38:01 +0100

    Permissions update

--- colleges/permissions.py
+++ colleges/permissions.py
@@ -2,10 +2,17 @@ from django.contrib.auth.decorators import user_passes_test
 
 
 def fellowship_required():
-    """Require user to have any Fellowship."""
+    """
+    Require user to have any Fellowship or Administrational permissions.
+    """
     def test(u):
         if u.is_authenticated():
             if hasattr(u, 'contributor') and u.contributor.fellowships.exists():
+                # Fellow
+                return True
+
+            if u.has_perm('scipost.can_view_pool'):
+                # Administrator
                 return True
         return False
     return user_passes_test(test)
