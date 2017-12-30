7d2727508287792662b687207b32c43add0fc307
Jorran de Wit <jorrandewit@outlook.com>
2017-12-30 21:41:34 +0100

    Update for full move to new server

--- SciPost_v1/settings/production.py
+++ SciPost_v1/settings/production.py
@@ -2,21 +2,18 @@ from .base import *
 
 # THE MAIN THING HERE
 DEBUG = False
-CERTFILE = get_secret("CERTFILE")
 ALLOWED_HOSTS = ['www.scipost.org', 'scipost.org']
 
 # Static and media
-STATIC_URL = 'https://scipost.org/static/'
-STATIC_ROOT = '/home/jscaux/webapps/scipost_static/'
-MEDIA_URL = 'https://scipost.org/media/'
-MEDIA_ROOT = '/home/jscaux/webapps/scipost_media/'
+STATIC_ROOT = '/home/scipost/webapps/scipost_static/'
+MEDIA_ROOT = '/home/scipost/webapps/scipost_media/'
 
 # Recaptcha
 RECAPTCHA_PUBLIC_KEY = get_secret("GOOGLE_RECAPTCHA_PUBLIC_KEY")
 RECAPTCHA_PRIVATE_KEY = get_secret("GOOGLE_RECAPTCHA_PRIVATE_KEY")
 
 WEBPACK_LOADER['DEFAULT']['CACHE'] = True
-WEBPACK_LOADER['DEFAULT']['BUNDLE_DIR_NAME'] = '/home/jscaux/webapps/scipost_static/bundles/'
+WEBPACK_LOADER['DEFAULT']['BUNDLE_DIR_NAME'] = '/home/scipost/webapps/scipost_static/bundles/'
 
 # Error reporting
 ADMINS = MANAGERS = (('J.S.Caux', 'J.S.Caux@uva.nl'), ('J.de Wit', 'jorrandewit@outlook.com'))
@@ -27,7 +24,6 @@ CSRF_COOKIE_SECURE = True
 
 # Email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
 EMAIL_HOST = get_secret("EMAIL_HOST")
 EMAIL_HOST_USER = get_secret("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = get_secret("EMAIL_HOST_PASSWORD")
@@ -39,7 +35,7 @@ SERVER_EMAIL = get_secret("SERVER_EMAIL")
 CROSSREF_LOGIN_ID = get_secret("CROSSREF_LOGIN_ID")
 CROSSREF_LOGIN_PASSWORD = get_secret("CROSSREF_LOGIN_PASSWORD")
 DOAJ_API_KEY = get_secret("DOAJ_API_KEY")
-HAYSTACK_CONNECTIONS['default']['PATH'] = '/home/jscaux/webapps/scipost/SciPost_v1/whoosh_index'
+HAYSTACK_CONNECTIONS['default']['PATH'] = '/home/scipost/webapps/scipost/SciPost_v1/whoosh_index'
 MAILCHIMP_API_USER = get_secret("MAILCHIMP_API_USER")
 MAILCHIMP_API_KEY = get_secret("MAILCHIMP_API_KEY")
 
