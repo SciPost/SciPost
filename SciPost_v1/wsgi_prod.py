7774cd5c9dccfd0fe150694f8067bca0b76c34d8
Jorran de Wit <jorrandewit@outlook.com>
2017-12-30 21:43:52 +0100

    Update for full move to new server

--- SciPost_v1/wsgi_prod.py
+++ SciPost_v1/wsgi_prod.py
@@ -6,6 +6,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SciPost_v1.settings.production_2")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SciPost_v1.settings.production")
 
 application = get_wsgi_application()
