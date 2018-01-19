89747568a7bc2e60ff920470905b933196e2bde4
Jorran de Wit <jorrandewit@outlook.com>
2018-01-19 01:32:31 +0100

    Update staging settings

--- SciPost_v1/settings/staging_release.py
+++ SciPost_v1/settings/staging_release.py
@@ -20,5 +20,7 @@ MEDIA_ROOT = '/home/jdewit/webapps/scipost_media/'
 WEBPACK_LOADER['DEFAULT']['CACHE'] = True
 WEBPACK_LOADER['DEFAULT']['BUNDLE_DIR_NAME'] = '/home/jdewit/webapps/scipost_static/bundles/'
 
-# Error reporting
-ADMINS = MANAGERS = (('J. de Wit', 'jorrandewit@outlook.com'), )
+
+# Logging
+LOGGING['handlers']['scipost_file_arxiv']['filename'] = '/home/jdewit/webapps/scipost/logs/arxiv.log'
+LOGGING['handlers']['scipost_file_doi']['filename'] = '/home/jdewit/webapps/scipost/logs/doi.log'
