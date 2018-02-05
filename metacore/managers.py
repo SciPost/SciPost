b478f0c12d187594ebcbc36dd9315a2e605c7be3
Boris Ponsioen <b.g.t.ponsioen@uva.nl>
2018-02-05 12:18:58 +0100

    Adds support for negative (exclusion) params in text search box, fixes PRL ISSN

--- metacore/managers.py
+++ metacore/managers.py
@@ -13,12 +13,19 @@ class CitableQuerySet(QuerySet):
         return self.only('doi', 'title', 'authors', 'metadata.is-referenced-by-count', 'publication_date', 'publisher')
 
     def prl(self):
-        return self.filter(metadata__ISSN='1079-7114')
+        return self.filter(metadata__ISSN='0031-9007')
 
     def omni_search(self, query, mode='and'):
         if mode == 'and':
             query_list = query.split(' ')
-            query_with_quotes = '"{0}"'.format('" "'.join(query_list))
+
+            # Treat words that start with '-' (exclude) differently
+            query_list_without_excludes = [q for q in query_list if not q[0] == '-']
+            query_with_quotes = '"{0}"'.format('" "'.join(query_list_without_excludes))
+
+            query_list_excludes = [q for q in query_list if q not in query_list_without_excludes]
+            query_with_quotes = query_with_quotes + ' ' + ' '.join(query_list_excludes) 
+
             return self.search_text(query_with_quotes)
         elif mode == 'or':
             return self.search_text(query)
