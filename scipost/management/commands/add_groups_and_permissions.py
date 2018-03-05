6e90032f520e7c83baa2fffdf54ca96b887994e6
Jorran de Wit <jorrandewit@outlook.com>
2018-03-05 20:34:40 +0100

    Update permissions

--- scipost/management/commands/add_groups_and_permissions.py
+++ scipost/management/commands/add_groups_and_permissions.py
@@ -248,6 +248,10 @@ class Command(BaseCommand):
             codename='can_view_all_funding_info',
             name='Can view all Funders info',
             content_type=content_type)
+        can_draft_publication, created = Permission.objects.get_or_create(
+            codename='can_draft_publication',
+            name='Can draft Publication',
+            content_type=content_type)
 
         # Documentation
         can_view_docs_scipost, created = Permission.objects.get_or_create(
@@ -325,6 +329,7 @@ class Command(BaseCommand):
             can_view_production,
             can_view_timesheets,
             can_publish_accepted_submission,
+            can_draft_publication,
             can_view_all_funding_info,
             can_attend_VGMs,
             can_manage_reports,
@@ -376,6 +381,7 @@ class Command(BaseCommand):
         ProductionSupervisors.permissions.set([
             can_assign_production_officer,
             can_take_decisions_related_to_proofs,
+            can_draft_publication,
             can_view_all_production_streams,
             can_run_proofs_by_authors,
             can_view_docs_scipost,
