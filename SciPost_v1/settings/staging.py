f5d3378cfa7c0986d96254a25c59eb741c27ab7a
Staging server <techsupport@scipost.org>
2018-04-06 07:44:56 +0000

    Update staging settings

--- SciPost_v1/settings/staging.py
+++ SciPost_v1/settings/staging.py
@@ -3,8 +3,8 @@
 from .base import *
 
 # THE MAIN THING HERE
-DEBUG = True
-ALLOWED_HOSTS = ['scipoststg.webfactional.com']
+DEBUG = False
+ALLOWED_HOSTS = ['scipoststg.webfactional.com', 'www.scipoststg.webfactional.com']
 
 # Static and media
 STATIC_ROOT = '/home/scipoststg/webapps/scipost_static/'
@@ -21,3 +21,8 @@ RECAPTCHA_PRIVATE_KEY = get_secret("GOOGLE_RECAPTCHA_PRIVATE_KEY")
 # Logging location
 LOGGING['handlers']['scipost_file_arxiv']['filename'] = '/home/scipoststg/webapps/scipost/logs/arxiv.log'
 LOGGING['handlers']['scipost_file_doi']['filename'] = '/home/scipoststg/webapps/scipost/logs/doi.log'
+
+# Cookies
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
