5fedcc269473fa077811c223ac9adbc98c8cd8e8
Boris Ponsioen <b.g.t.ponsioen@uva.nl>
2018-04-16 11:59:58 +0200

    Adds initial metacore admin action

--- metacore/admin.py
+++ metacore/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .services import get_crossref_test
 
 # Register your models here.
+def import_data_from_crossref(modeladmin, request, queryset):
+    get_crossref_test()
