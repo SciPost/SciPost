a6beabdc5b4c39d510f3a078ee1e49859e317bcf
Jorran de Wit <jorrandewit@outlook.com>
2018-04-29 13:06:05 +0200

    Debug 1.
    Found 1 typo + 1 wrong variable.

--- submissions/management/commands/send_refereeing_reminders.py
+++ submissions/management/commands/send_refereeing_reminders.py
@@ -13,6 +13,7 @@ from ...models import Submission
 
 class Command(BaseCommand):
     help = 'Sends all email reminders needed for Submissions undergoing refereeing'
+
     def handle(self, *args, **options):
         for submission in Submission.objects.open_for_reporting():
             # Send reminders to referees who have not responded:
@@ -33,7 +34,7 @@ class Command(BaseCommand):
                     invitation.date_last_reminded = timezone.now()
                     invitation.save()
                 # second (and final) reminder after 4 days
-                if workdays_between(invitation.date_invited, timezone.now()) == 4:
+                elif workdays_between(invitation.date_invited, timezone.now()) == 4:
                     if invitation.referee:
                         mail_sender = DirectMailUtil(
                             mail_code='referees/invite_contributor_to_referee_reminder2',
@@ -49,14 +50,13 @@ class Command(BaseCommand):
                     invitation.save()
                 # after 6 days of no response, EIC is automatically emailed
                 # with the suggestion of removing and replacing this referee
-                if workdays_between(invitation.date_invited, timezone.now()) == 6:
+                elif workdays_between(invitation.date_invited, timezone.now()) == 6:
                     mail_sender = DirectMailUtil(
-                        mail_code='eic/referee_unresponsive',
-                        instance=invitation)
+                        mail_code='eic/referee_unresponsive', instance=invitation)
                     mail_sender.send()
             # one week before refereeing deadline: auto email reminder to ref
             if workdays_between(timezone.now(), submission.reporting_deadline) == 5:
                 for invitation in submission.refereeing_invitations.in_process():
-                    mail_sender = DirectMailUtil(mail_code='referees/remind_referee_deadline_1week',
-                                                 instance=invitation)
+                    mail_sender = DirectMailUtil(
+                        mail_code='referees/remind_referee_deadline_1week', instance=invitation)
                     mail_sender.send()
