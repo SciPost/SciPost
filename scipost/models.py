f6c51fc628a0a239589139ff632ce5926df7cd99
Jorran de Wit <jorrandewit@outlook.com>
2018-05-10 11:43:37 +0200

    Quickfix: hash is guessable

--- scipost/models.py
+++ scipost/models.py
@@ -108,7 +108,7 @@ class Contributor(models.Model):
             feed += random.choice(string.ascii_letters)
         feed = feed.encode('utf8')
         salt = self.user.username.encode('utf8')
-        self.activation_key = hashlib.sha1(salt + salt).hexdigest()
+        self.activation_key = hashlib.sha1(salt + feed).hexdigest()
         self.key_expires = datetime.datetime.now() + datetime.timedelta(days=2)
 
     def expertises_as_string(self):
