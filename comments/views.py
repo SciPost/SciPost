aace02dfd7129f12a2fa19b47eb54600272710a2
J.-S. Caux <J.S.Caux@uva.nl>
2018-05-27 16:46:55 +0200

    Removed CommentUtils. New emails for Comments NEED COMPLETION
    The DirectMailUtil needs to support passing extra context
    before this can be considered functional.

--- comments/views.py
+++ comments/views.py
@@ -16,7 +16,7 @@ import strings
 from .constants import EXTENTIONS_IMAGES, EXTENTIONS_PDF
 from .models import Comment
 from .forms import CommentForm, VetCommentForm
-from .utils import CommentUtils, validate_file_extention
+from .utils import validate_file_extention
 
 from commentaries.models import Commentary
 from mails.utils import DirectMailUtil
@@ -87,8 +87,10 @@ def vet_submitted_comment(request, comment_id):
                 content_object.add_event_for_eic('A Comment has been accepted.')
                 content_object.add_event_for_author('A new Comment has been added.')
                 if not comment.is_author_reply:
-                    SubmissionUtils.load({'submission': content_object})
-                    SubmissionUtils.send_author_comment_received_email()
+                    mail_sender = DirectMailUtil(
+                        mail_code='authors/inform_authors_comment_received',
+                        instance=content_object)
+                    mail_sender.send()
             elif isinstance(content_object, Report):
                 # Add events to related Submission and send mail to author of the Submission
                 content_object.submission.add_event_for_eic('A Comment has been accepted.')
@@ -99,13 +101,23 @@ def vet_submitted_comment(request, comment_id):
                         mail_code='referees/inform_referee_authors_replied_to_report',
                         instance=content_object)
                     mail_sender.send()
-                else:
-                    SubmissionUtils.load({'submission': content_object.submission})
-                    SubmissionUtils.send_author_comment_received_email()
+                else: # this is a Comment on the Report from another Contributor
+                    # Email Report author: Contributor has commented the Report
+                    mail_sender = DirectMailUtil(
+                        mail_code='referees/inform_referee_contributor_commented_report',
+                        instance=content_object)
+                    mail_sender.send()
+                    # Email submission authors: Contributor has commented the Report
+                    mail_sender = DirectMailUtil(
+                        mail_code='authors/inform_authors_contributor_commented_report',
+                        instance=content_object)
+                    mail_sender.send()
 
-            # Send emails
-            CommentUtils.load({'comment': comment})
-            CommentUtils.email_comment_vet_accepted_to_author()
+            # In all cases, email the comment author
+            mail_sender = DirectMailUtil(
+                mail_code='commenters/inform_commenter_comment_vetted',
+                instance=comment)
+            mail_sender.send()
 
         elif form.cleaned_data['action_option'] == '2':
             # The comment request is simply rejected
@@ -115,9 +127,12 @@ def vet_submitted_comment(request, comment_id):
             comment.save()
 
             # Send emails
-            CommentUtils.load({'comment': comment})
-            CommentUtils.email_comment_vet_rejected_to_author(
-                email_response=form.cleaned_data['email_response_field'])
+            mail_sender = DirectMailUtil(
+                mail_code='commenters/inform_commenter_comment_rejected',
+                instance=comment,
+                email_response=form.cleaned_data['email_response_field']) # TODO: needs kwargs to mail template
+            mail_sender.send()
+
 
             if isinstance(comment.content_object, Submission):
                 # Add event if commented to Submission
--- submissions/utils.py
+++ submissions/utils.py
@@ -988,48 +988,6 @@ class SubmissionUtils(BaseMailUtil):
         emailmessage.attach_alternative(html_version, 'text/html')
         emailmessage.send(fail_silently=False)
 
-    @classmethod
-    def send_author_comment_received_email(cls):
-        """ Requires loading 'submission' attribute. """
-        email_text = ('Dear ' + cls.submission.submitted_by.get_title_display() + ' ' +
-                      cls.submission.submitted_by.user.last_name +
-                      ', \n\nA Comment has been posted on your recent Submission to SciPost,\n\n' +
-                      cls.submission.title + ' by ' + cls.submission.author_list + '.'
-                      '\n\nYou can view it at the Submission Page '
-                      'https://scipost.org/submission/'
-                      + cls.submission.arxiv_identifier_w_vn_nr + '.'
-                      '\n\nWe thank you very much for your contribution.'
-                      '\n\nSincerely,' +
-                      '\n\nThe SciPost Team.')
-        email_text_html = (
-            '<p>Dear {{ auth_title }} {{ auth_last_name }},</p>'
-            '<p>A Comment has been posted on your recent Submission to SciPost,</p>'
-            '<p>{{ sub_title }}</p>\n<p>by {{ author_list }}.</p>'
-            '\n<p>You can view it at the '
-            '<a href="https://scipost.org/submission/{{ arxiv_identifier_w_vn_nr }}">'
-            'Submission\'s Page</a>.</p>'
-            '\n<p>We thank you very much for your contribution.</p>'
-            '<p>Sincerely,</p>'
-            '<p>The SciPost Team.</p>')
-        email_context = {
-            'auth_title': cls.submission.submitted_by.get_title_display(),
-            'auth_last_name': cls.submission.submitted_by.user.last_name,
-            'sub_title': cls.submission.title,
-            'author_list': cls.submission.author_list,
-            'arxiv_identifier_w_vn_nr': cls.submission.arxiv_identifier_w_vn_nr,
-        }
-        email_text_html += '<br/>' + EMAIL_FOOTER
-        html_template = Template(email_text_html)
-        html_version = html_template.render(Context(email_context))
-        emailmessage = EmailMultiAlternatives(
-            'SciPost: Comment received on your Submission', email_text,
-            'SciPost Editorial Admin <submissions@scipost.org>',
-            [cls.submission.submitted_by.user.email],
-            bcc=[cls.submission.editor_in_charge.user.email,
-                 'submissions@scipost.org'],
-            reply_to=['submissions@scipost.org'])
-        emailmessage.attach_alternative(html_version, 'text/html')
-        emailmessage.send(fail_silently=False)
 
     @classmethod
     def send_communication_email(cls):
