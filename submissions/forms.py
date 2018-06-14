36f8ab79c2914d6afc46f6513c23ec75d0978bea
Jorran de Wit <jorrandewit@outlook.com>
2018-06-14 15:18:19 +0200

    Fix EIC assigned on rejection

--- submissions/forms.py
+++ submissions/forms.py
@@ -562,7 +562,6 @@ class EditorialAssignmentForm(forms.ModelForm):
             Submission.objects.filter(id=self.submission.id).update(
                 refereeing_cycle=CYCLE_DEFAULT,
                 status=STATUS_EIC_ASSIGNED,
-                editor_in_charge=self.request.user.contributor,
                 reporting_deadline=deadline,
                 open_for_reporting=True,
                 open_for_commenting=True,
@@ -575,7 +574,6 @@ class EditorialAssignmentForm(forms.ModelForm):
             Submission.objects.filter(id=self.submission.id).update(
                 refereeing_cycle=CYCLE_DIRECT_REC,
                 status=STATUS_EIC_ASSIGNED,
-                editor_in_charge=self.request.user.contributor,
                 reporting_deadline=timezone.now(),
                 open_for_reporting=False,
                 open_for_commenting=True,
@@ -584,6 +582,8 @@ class EditorialAssignmentForm(forms.ModelForm):
 
         if self.has_accepted_invite():
             # Implicitly or explicity accept the assignment and deprecate others.
+            Submission.objects.filter(id=self.submission.id).update(
+                editor_in_charge=self.request.user.contributor,)
             recommendation.accepted = True
             EditorialAssignment.objects.filter(submission=self.submission, accepted=None).exclude(
                 id=recommendation.id).update(deprecated=True)
