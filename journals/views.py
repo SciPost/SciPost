a73b8e530f39a05be694d98897af2344a6dec3ef
J.-S. Caux <J.S.Caux@uva.nl>
2018-06-16 20:57:24 +0200

    Force save after update to check if this is the bug

--- journals/views.py
+++ journals/views.py
@@ -872,6 +872,7 @@ def generic_metadata_xml_deposit(request, **kwargs):
 
     if not _object.doi_label:
         _object.create_doi_label()
+        _object.save()
 
     # create a doi_batch_id
     salt = ""
