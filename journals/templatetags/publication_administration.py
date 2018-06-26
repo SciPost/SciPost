babd941deb8c904935770d029dffa9422584732d
J.-S. Caux <J.S.Caux@uva.nl>
2018-06-26 21:59:53 +0200

    Add template filter to check author ordering

--- journals/templatetags/publication_administration.py
+++ journals/templatetags/publication_administration.py
@@ -13,3 +13,17 @@ def has_all_author_relations(publication):
     Check if all authors are added to the Publication object, just by counting.
     """
     return len(publication.author_list.split(',')) == publication.authors.count()
+
+
+@register.filter
+def authors_in_right_order(publication):
+    """
+    Checks if all author orderings correspond to those in author list.
+    """
+    if not has_all_author_relations(publication):
+        return False
+    list_of_authors = publication.author_list.split(',')
+    for author in publication.authors.all():
+        if author.last_name not in list_of_authors[author.order]:
+            return False
+    return True
