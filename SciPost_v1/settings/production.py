3f1f06973326e6dc1497a165413722a28b723767
J.-S. Caux <J.S.Caux@uva.nl>
2018-07-18 20:18:07 +0200

    Change email for Crossref deposits to edadmin (instead of admin)

--- SciPost_v1/settings/production.py
+++ SciPost_v1/settings/production.py
@@ -39,7 +39,7 @@ SERVER_EMAIL = get_secret("SERVER_EMAIL")
 CROSSREF_LOGIN_ID = get_secret("CROSSREF_LOGIN_ID")
 CROSSREF_LOGIN_PASSWORD = get_secret("CROSSREF_LOGIN_PASSWORD")
 CROSSREF_DEBUG = False
-CROSSREF_DEPOSIT_EMAIL = 'admin@scipost.org'
+CROSSREF_DEPOSIT_EMAIL = 'edadmin@scipost.org'
 
 DOAJ_API_KEY = get_secret("DOAJ_API_KEY")
 HAYSTACK_CONNECTIONS['default']['PATH'] = '/home/scipost/webapps/scipost/scipost_v1/whoosh_index'
