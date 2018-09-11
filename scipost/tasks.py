e26e69382cc376f3bfedde02d344175627ece2e1
Jorran de Wit <jorrandewit@outlook.com>
2018-09-11 10:55:55 +0200

    Second try

---
+++ scipost/tasks.py
@@ -0,0 +1,20 @@
+__copyright__ = "Copyright 2016-2018, Stichting SciPost (SciPost Foundation)"
+__license__ = "AGPL v3"
+
+
+from django.core.mail import send_mail
+
+
+from SciPost_v1.celery import app
+
+
+@app.task(bind=True)
+def test_celery_using_mail(self):
+    """Just testing the production server here."""
+    send_mail(
+        'Test subject',
+        'Received this mail?',
+        'noreply@scipost.org',
+        ['jorrandewit@scipost.org'],
+        fail_silently=False,
+    )
