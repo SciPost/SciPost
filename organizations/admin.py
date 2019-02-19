457860ce6e38a1f66d62f5353291d9f5e379de8c
J.-S. Caux <J.S.Caux@uva.nl>
2019-02-19 20:23:13 +0100

    Add admin for new classes in organizations app

--- organizations/admin.py
+++ organizations/admin.py
@@ -4,12 +4,32 @@ __license__ = "AGPL v3"
 
 from django.contrib import admin
 
-from .models import Organization
+from .models import Organization, OrganizationEvent, ContactPerson, Contact, ContactRole
 
 
+class OrganizationEventInline(admin.TabularInline):
+    model = OrganizationEvent
+    extra = 0
+
+class ContactPersonInline(admin.TabularInline):
+    model = ContactPerson
+    extra = 0
 
 class OrganizationAdmin(admin.ModelAdmin):
+    inlines = [OrganizationEventInline, ContactPersonInline,]
     search_fields = ['name', 'acronym']
 
 
 admin.site.register(Organization, OrganizationAdmin)
+
+
+class ContactRoleInline(admin.TabularInline):
+    model = ContactRole
+    extra = 0
+
+class ContactAdmin(admin.ModelAdmin):
+    inlines = [ContactRoleInline,]
+    search_fields = ['user__last_name', 'user__first_name', 'user__email']
+
+
+admin.site.register(Contact, ContactAdmin)
