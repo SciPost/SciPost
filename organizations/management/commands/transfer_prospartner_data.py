d00085955f66cadd217c7d6638af4c211c6fd165
J.-S. Caux <J.S.Caux@uva.nl>
2019-02-25 14:57:06 +0100

    Tweak prospartner data transfer

--- organizations/management/commands/transfer_prospartner_data.py
+++ organizations/management/commands/transfer_prospartner_data.py
@@ -2,6 +2,7 @@ __copyright__ = "Copyright © Stichting SciPost (SciPost Foundation)"
 __license__ = "AGPL v3"
 
 
+from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 
 from partners.models import ProspectivePartner
@@ -27,13 +28,18 @@ class Command(BaseCommand):
                     role=proscontact.role)
                 contact.save()
                 proscontact.delete()
+            oldman = User.objects.get(email='J.S.Caux@uva.nl')
             for prosevent in prospartner.prospectivepartnerevent_set.all():
+                if prosevent.noted_by:
+                    noted_by = prosevent.noted_by.user
+                else:
+                    noted_by = oldman
                 event = OrganizationEvent(
                     organization=prospartner.organization,
                     event=prosevent.event,
                     comments=prosevent.comments,
                     noted_on=prosevent.noted_on,
-                    noted_by=prosevent.noted_by.user)
+                    noted_by=noted_by)
                 event.save()
                 prosevent.delete()
             prospartner.delete()
