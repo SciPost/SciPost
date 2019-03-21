3c4a47fc2208995ac1bbdc85063328b5781e9020
J.-S. Caux <J.S.Caux@uva.nl>
2019-03-21 13:46:52 +0100

    Remove circular import

--- journals/views.py
+++ journals/views.py
@@ -46,7 +46,6 @@ from .utils import JournalUtils
 from comments.models import Comment
 from funders.forms import FunderSelectForm, GrantSelectForm
 from funders.models import Grant
-from mails.views import MailEditorSubview
 from ontology.models import Topic
 from ontology.forms import SelectTopicForm
 from organizations.models import Organization
