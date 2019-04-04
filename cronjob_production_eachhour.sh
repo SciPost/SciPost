c8bbd3300496465964023556813c3a34082643f2
Jorran de Wit <jorrandewit@outlook.com>
2019-04-04 17:55:19 +0200

    Start search cron

--- cronjob_production_eachhour.sh
+++ cronjob_production_eachhour.sh
@@ -8,3 +8,6 @@ source venv/bin/activate
 # Do tasks
 python3 manage.py check_celery
 python3 manage.py update_coi_via_arxiv
+
+# Do a update_index of the last hour
+python3 manage.py update_index -r -v 0 -a 1
--- cronjob_production_sundays.sh
+++ cronjob_production_sundays.sh
@@ -7,3 +7,6 @@ cd /home/scipost/webapps/scipost/scipost_v1
 source venv/bin/activate
 
 python3 manage.py update_citedby
+
+# Do a full update_index when maybe something has slipped through during the week somehow..?
+python3 manage.py update_index -r -v 0
