7e79f95b3125c609000960d9d12d662f1bd52b96
J.-S. Caux <J.S.Caux@uva.nl>
2019-04-05 20:08:32 +0200

    Debug again

--- journals/signals.py
+++ journals/signals.py
@@ -15,8 +15,8 @@ def notify_manuscript_published(sender, instance, created, **kwargs):
     """
     if instance.is_published:
         editorial_administration = Group.objects.get(name='Editorial Administrators')
-        for profile in instance.authors.all():
-            if profile.has_active_contributor:
-                notify.send(sender=sender, recipient=profile.contributor.user,
+        for author in instance.authors.all():
+            if author.profile.has_active_contributor:
+                notify.send(sender=sender, recipient=author.profile.contributor.user,
                             actor=editorial_administration,
                             verb=' published your manuscript.', target=instance)
