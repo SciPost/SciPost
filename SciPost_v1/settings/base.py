6ef32fa6474343873469fe7db5d4163e0f48dbba
J.-S. Caux <J.S.Caux@uva.nl>
2019-05-04 15:32:54 +0200

    Add Crossref as IMG_SRC

--- SciPost_v1/settings/base.py
+++ SciPost_v1/settings/base.py
@@ -198,7 +198,7 @@ X_FRAME_OPTIONS = 'DENY'
 REFERRER_POLICY = 'same-origin'
 CSP_FONT_SRC = ("'self'", 'fonts.gstatic.com', 'cdnjs.cloudflare.com')
 CSP_FRAME_SRC = ('www.google.com')
-CSP_IMG_SRC = ("'self'", 'licensebuttons.net', 'crossmark-cdn.crossref.org')
+CSP_IMG_SRC = ("'self'", 'assets.crossref.org', 'licensebuttons.net', 'crossmark-cdn.crossref.org')
 CSP_SCRIPT_SRC = ("'self'", "'unsafe-inline'", 'ajax.googleapis.com', 'cdnjs.cloudflare.com',
                   'crossmark-cdn.crossref.org', 'www.recaptcha.net', 'www.gstatic.com')
 CSP_STYLE_SRC = ("'self'", "'unsafe-inline'", 'ajax.googleapis.com',
