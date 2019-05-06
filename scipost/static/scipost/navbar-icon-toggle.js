5ccdca76d44b1cfe72fdf2fc3b30219470fc8ea1
J.-S. Caux <J.S.Caux@uva.nl>
2019-05-06 15:16:49 +0200

    Move the navbar toggler script to event listener in separate file

--- scipost/static/scipost/navbar-icon-toggle.js
+++ scipost/static/scipost/navbar-icon-toggle.js
@@ -1,3 +1,8 @@
 function navbarIconToggle(x) {
     x.classList.toggle("change");
 }
+
+document.addEventListener('DOMContentReady', function () {
+    document.getElementById('navbar-toggler-icon')
+        .addEventListener('click', navbaraIconToggle(this));
+});
