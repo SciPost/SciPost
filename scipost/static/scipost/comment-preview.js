740d655846bd7fcf7ef23dedb47a4c4234de019b
J.-S. Caux <J.S.Caux@uva.nl>
2019-06-22 09:59:14 +0200

    Improve comment automarkup, include remarks_for_editors

--- scipost/static/scipost/comment-preview.js
+++ scipost/static/scipost/comment-preview.js
@@ -1,17 +1,26 @@
 $(document).ready(function() {
 
     $('#submitButton').hide();
+    $('#preview-remarks_for_editors-div').hide();
 
     $("#id_comment_text").on('keyup', function(){
 	$('#runPreviewButton').show();
 	$('#preview-comment_text').css('background', '#feebce');
 	$('#submitButton').hide();
     });
+    $("#id_remarks_for_editors").on('keyup', function(){
+	$('#runPreviewButton').show();
+	$('#preview-remarks_for_editors-div').show();
+	$('#preview-remarks_for_editors').css('background', '#feebce');
+	$('#submitButton').hide();
+    });
 
     $('#runPreviewButton').on('click', function(){
 	fetchMarkupPreview('comment_text');
+	fetchMarkupPreview('remarks_for_editors');
     	$('#runPreviewButton').hide();
     	$('#preview-comment_text').css('background', '#ffffff');
+    	$('#preview-remarks_for_editors').css('background', '#ffffff');
     	$('#submitButton').show();
     }).trigger('change');
 
