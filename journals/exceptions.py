e18d23ee3cbf22e5346fa33e6691c0421b7be923
J.-S. Caux <J.S.Caux@uva.nl>
2019-08-20 16:58:49 +0200

    Remove unused exception

--- journals/exceptions.py
+++ journals/exceptions.py
@@ -2,14 +2,6 @@ __copyright__ = "Copyright © Stichting SciPost (SciPost Foundation)"
 __license__ = "AGPL v3"
 
 
-class JournalNameError(Exception):
-    def __init__(self, name):
-        self.name = name
-
-    def __str__(self):
-        return self.name
-
-
 class PaperNumberError(Exception):
     def __init__(self, nr):
         self.nr = nr
