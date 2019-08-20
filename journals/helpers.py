4f7aaa67f1943c92a31be633392d362e2823b747
J.-S. Caux <J.S.Caux@uva.nl>
2019-08-20 15:57:32 +0200

    Remove deprecated issue_doi_label_from_doi_label helper method

--- journals/helpers.py
+++ journals/helpers.py
@@ -7,16 +7,6 @@ import re
 from .exceptions import PaperNumberError
 
 
-def issue_doi_label_from_doi_label(doi_label):
-    """
-    Strip the last digits block from the label.
-    """
-    m = re.match(r'[a-zA-Z]+.[0-9]+.[0-9]+', doi_label)
-    s = m.start()
-    e = m.end()
-    return doi_label[s:e]
-
-
 def paper_nr_string(nr):
     if nr < 10:
         return '00' + str(nr)
