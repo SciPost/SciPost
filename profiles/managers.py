449b854928678bd85a746e6fa7f479d207876a09
J.-S. Caux <J.S.Caux@uva.nl>
2019-11-14 15:35:57 +0100

    Add (case-insensitive) email duplicates flagging for Profiles

--- profiles/managers.py
+++ profiles/managers.py
@@ -4,7 +4,7 @@ __license__ = "AGPL v3"
 
 from django.db import models
 from django.db.models import Count
-from django.db.models.functions import Concat
+from django.db.models.functions import Concat, Lower
 
 
 class ProfileQuerySet(models.QuerySet):
@@ -21,16 +21,29 @@ class ProfileQuerySet(models.QuerySet):
     def potential_duplicates(self):
         """
         Returns only potential duplicate Profiles (as identified by first and
-        last names).
+        last names, and separately by (case-insensitive) email).
         """
+        # Start by treating name duplicates, excluding marked Profile non-duplicates
         from .models import ProfileNonDuplicates
         profiles = self.annotate(full_name=Concat('last_name', 'first_name'))
         nonduplicate_full_names = [dup.full_name for dup in ProfileNonDuplicates.objects.all()]
-        duplicates = profiles.values('full_name').annotate(
-            nr_count=Count('full_name')
-        ).filter(nr_count__gt=1).exclude(full_name__in=nonduplicate_full_names)
-        return profiles.filter(full_name__in=[item['full_name'] for item in duplicates]
-                              ).order_by('last_name', 'first_name', '-id')
+        duplicates_by_full_name = profiles.values('full_name').annotate(
+            nr_count=Count('full_name')).filter(nr_count__gt=1
+            ).exclude(full_name__in=nonduplicate_full_names)
+        from .models import ProfileEmail
+        # Now for email duplicates. Because of case-insensitivity, we need some gymnastics
+        pel = ProfileEmail.objects.annotate(email_lower=Lower('email'))
+        # Build list of all duplicate lowercased emails
+        duplicate_emails = [pe['email_lower'] for pe in pel.values('email_lower').annotate(
+            nel=Count('email_lower')).filter(nel__gt=1)]
+        # Then determine all ids of related Profiles with an email in this list
+        ids_of_duplicates_by_email = [pe.profile.id for pe in pel.filter(
+            email_lower__in=duplicate_emails)]
+        # Now return list of potential duplicates
+        return profiles.filter(
+            models.Q(full_name__in=[item['full_name'] for item in duplicates_by_full_name]) |
+            models.Q(id__in=ids_of_duplicates_by_email)
+        ).order_by('last_name', 'first_name', '-id')
 
     def specialties_overlap(self, discipline, expertises=[]):
         """
