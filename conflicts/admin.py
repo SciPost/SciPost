1ae0f031c88bc6446cd22120486a7c6501ab694a
J.-S. Caux <J.S.Caux@uva.nl>
2019-11-15 10:45:40 +0100

    Improve conflicts admin (while waiting for new COI system)

--- conflicts/admin.py
+++ conflicts/admin.py
@@ -8,7 +8,13 @@ from .models import ConflictOfInterest
 
 
 class ConflictAdmin(admin.ModelAdmin):
-    search_fields = ('header', 'profile__last_name', 'related_profile__last_name')
+    search_fields = (
+        'header',
+        'profile__last_name',
+        'related_profile__last_name',
+        'related_submissions__title',
+        'related_submissions__preprint__identifier_w_vn_nr'
+    )
     list_filter = ('status', 'type')
     list_display = ('header', 'profile', 'related_profile', 'status', 'type')
 
