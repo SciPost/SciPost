c0c72d9eee73341575ed4ce4f003d6441c04aef3
J.-S. Caux <J.S.Caux@uva.nl>
2019-11-24 20:31:53 +0100

    Fails again

--- webpack.test.config.js
+++ webpack.test.config.js
@@ -1,11 +1,11 @@
-var webpack = require("webpack");
-var path_bundles = __dirname + '/static_bundles/bundles';
+const webpack = require("webpack");
+const path_bundles = __dirname + '/static_bundles/bundles';
 
 module.exports = {
     mode: 'development',
-    context: __dirname,
     entry: {
         main: [
+	    "tether",
             "bootstrap-loader",
             "./scipost/static/scipost/assets/js/dynamic_loading.js",
             "./scipost/static/scipost/assets/js/scripts.js",
@@ -20,17 +20,38 @@ module.exports = {
         publicPath: '/static/bundles/',
         filename: "js/[name]-[hash].js",
     },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+	    'window.jQuery': 'jquery',
+            Tether: 'tether',
+            'window.Tether': 'tether',
+            Popper: ['popper.js', 'default'],
+	    Alert: "exports-loader?Alert!bootstrap/js/dist/alert",
+	    Button: "exports-loader?Button!bootstrap/js/dist/button",
+	    Carousel: "exports-loader?Carousel!bootstrap/js/dist/carousel",
+	    Collapse: "exports-loader?Collapse!bootstrap/js/dist/collapse",
+	    Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
+	    Modal: "exports-loader?Modal!bootstrap/js/dist/modal",
+	    Popover: "exports-loader?Popover!bootstrap/js/dist/popover",
+	    Scrollspy: "exports-loader?Scrollspy!bootstrap/js/dist/scrollspy",
+	    Tab: "exports-loader?Tab!bootstrap/js/dist/tab",
+            Tooltip: "exports-loader?Tooltip!bootstrap/js/dist/tooltip",
+            Util: 'exports-loader?Util!bootstrap/js/dist/util',
+        }),
+    ],
     module: {
 	rules: [
 	    {
-		test: require.resolve('jquery'),
-		use: [{
-		    loader: 'expose-loader',
-		    options: 'jQuery'
-		},{
-		loader: 'expose-loader',
-		options: '$'
-		}]
+	    	test: require.resolve('jquery'),
+	    	use: [{
+	    	    loader: 'expose-loader',
+	    	    options: 'jQuery'
+	    	},{
+	    	loader: 'expose-loader',
+	    	options: '$'
+	    	}]
 	    },
             {
                 test: /\.css$/,
@@ -42,26 +63,4 @@ module.exports = {
             }
 	]
     },
-    plugins: [
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-            Tether: 'tether',
-            'window.Tether': 'tether',
-            Tooltip: "exports-loader?Tooltip!bootstrap/js/dist/tooltip",
-            Util: 'exports-loader?Util!bootstrap/js/dist/util',
-            Popper: ['popper.js', 'default'],
-	    Alert: "exports-loader?Alert!bootstrap/js/dist/alert",
-	    Button: "exports-loader?Button!bootstrap/js/dist/button",
-	    Carousel: "exports-loader?Carousel!bootstrap/js/dist/carousel",
-	    Collapse: "exports-loader?Collapse!bootstrap/js/dist/collapse",
-	    Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
-	    Modal: "exports-loader?Modal!bootstrap/js/dist/modal",
-	    Popover: "exports-loader?Popover!bootstrap/js/dist/popover",
-	    Scrollspy: "exports-loader?Scrollspy!bootstrap/js/dist/scrollspy",
-	    Tab: "exports-loader?Tab!bootstrap/js/dist/tab",
-	    Tooltip: "exports-loader?Tooltip!bootstrap/js/dist/tooltip",
-	    Util: "exports-loader?Util!bootstrap/js/dist/util",
-        }),
-    ],
 }
