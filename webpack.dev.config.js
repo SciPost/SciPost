dbe0cd3595a9478e0b20a798ffdf64a0d1e2cf40
J.-S. Caux <J.S.Caux@uva.nl>
2019-12-05 09:39:16 +0100

    Add vue and bootstrap-vue to webpack

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -11,9 +11,9 @@ module.exports = {
         main: [
 	    "tether",
             "bootstrap-loader",
+            "./scipost/static/scipost/assets/js/main.js",
             "./scipost/static/scipost/assets/js/dynamic_loading.js",
             "./scipost/static/scipost/assets/js/scripts.js",
-            "./scipost/static/scipost/assets/js/vue.js",
         ],
         homepage: [
             "./scipost/static/scipost/assets/js/fader.js",
