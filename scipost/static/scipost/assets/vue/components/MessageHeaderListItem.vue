0efef8e5f7bc4c04f1dcf8d3c48d11afe2c19d2f
J.-S. Caux <J.S.Caux@uva.nl>
2019-12-10 05:58:19 +0100

    Work on styling

--- scipost/static/scipost/assets/vue/components/MessageHeaderListItem.vue
+++ scipost/static/scipost/assets/vue/components/MessageHeaderListItem.vue
@@ -1,7 +1,16 @@
 <template>
-  <b-list-group-item :id="'item' + [[ message.uuid ]]">
-    On {{ message.data.Date }}, from {{ message.data.from }}
-  </b-list-group-item>
+<b-list-group-item :id="'item' + [[ message.uuid ]]" class="py-0">
+  <b-row>
+    <b-col>
+      On: {{ message.datetimestamp }}
+      <br>
+      From: {{ message.data.from }}
+    </b-col>
+    <b-col>
+      Subject: {{ message.data.subject }}
+    </b-col>
+  </b-row>
+</b-list-group-item>
 </template>
 
 <script>
--- scipost/static/scipost/assets/vue/message_list.js
+++ scipost/static/scipost/assets/vue/message_list.js
@@ -3,7 +3,7 @@ import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
 Vue.use(BootstrapVue);
 
-import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 
