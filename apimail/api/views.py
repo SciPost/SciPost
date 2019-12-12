d04f6e4a8ee12f419608cb82a2a69f51e698df57
J.-S. Caux <J.S.Caux@uva.nl>
2019-12-12 16:47:10 +0100

    Add message list filtering

--- apimail/api/views.py
+++ apimail/api/views.py
@@ -2,12 +2,48 @@ __copyright__ = "Copyright © Stichting SciPost (SciPost Foundation)"
 __license__ = "AGPL v3"
 
 
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from django.db.models import Q
 
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.permissions import AllowAny, IsAdminUser
+from rest_framework import filters
 
 from ..models import Event, StoredMessage
 from .serializers import EventSerializer, StoredMessageSerializer
+
+
+class StoredMessageFilterBackend(filters.BaseFilterBackend):
+    """
+    Filter that only allows users to see their own objects.
+    """
+    def filter_queryset(self, request, queryset, view):
+        queryset = StoredMessage.objects.all()
+        queryfilter = Q()
+
+        _from = request.query_params.get('from', None)
+        if _from is not None:
+            queryfilter = queryfilter | Q(data__from__icontains=_from)
+        subject = request.query_params.get('subject', None)
+        if subject is not None:
+            queryfilter = queryfilter | Q(data__subject__icontains=subject)
+        recipients = request.query_params.get('recipients', None)
+        if recipients is not None:
+            queryfilter = queryfilter | Q(data__recipients__icontains=recipients)
+        # For full-text searches through body-plain / body-html, we use a
+        # raw SQL query since Django ORM does not support hyphenated lookups,
+        # and since Mailgun uses hyphenated keys in its JSON responses.
+        body = request.query_params.get('body', None)
+        if body is not None:
+            query_raw = (
+                "SELECT apimail_storedmessage.id FROM apimail_storedmessage "
+                "WHERE UPPER((apimail_storedmessage.data ->> %s)::text) LIKE UPPER(%s) "
+                "OR UPPER((apimail_storedmessage.data ->> %s)::text) LIKE UPPER(%s) "
+                "ORDER BY apimail_storedmessage.datetimestamp DESC;")
+            sm_ids = [sm.id for sm in StoredMessage.objects.raw(
+                query_raw, ['body-plain', '%%%s%%' % body, 'body-html', '%%%s%%' % body])]
+            queryfilter = queryfilter | Q(pk__in=sm_ids)
+
+        return queryset.filter(queryfilter).filter_for_user(request.user)
 
 
 class EventListAPIView(ListAPIView):
@@ -29,12 +65,11 @@ class StoredMessageListAPIView(ListAPIView):
     permission_classes = (IsAdminUser,)
     serializer_class = StoredMessageSerializer
     lookup_field = 'uuid'
+    filter_backends = [StoredMessageFilterBackend,]
 
 
 class StoredMessageRetrieveAPIView(RetrieveAPIView):
     permission_classes = (IsAdminUser,)
     serializer_class = StoredMessageSerializer
     lookup_field = 'uuid'
-
-    def get_queryset(self):
-        return StoredMessage.objects.filter_for_user(self.request.user)
+    filter_backends = [StoredMessageFilterBackend,]
--- apimail/static/apimail/assets/vue/components/MessagesTable.vue
+++ apimail/static/apimail/assets/vue/components/MessagesTable.vue
@@ -1,16 +1,66 @@
 <template>
 <div class="overflow-auto">
+  <b-card bg-variant="light">
+    <b-row class="mb-0">
+      <b-col>
+	<b-form-group
+	  label="Filter"
+	  label-cols-sm="3"
+          label-align-sm="right"
+          label-size="sm"
+	  >
+	  <b-input-group size="sm">
+	    <b-form-input
+	      v-model="filter"
+	      type="search"
+	      id="filterInput"
+	      placeholder="Type to search"
+	      >
+	    </b-form-input>
+	    <b-input-group-append>
+              <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
+	    </b-input-group-append>
+	  </b-input-group>
+	</b-form-group>
+      </b-col>
+      <b-col class="mb-0">
+	<b-form-group
+          label="Filter on:"
+	  label-cols-sm="3"
+          label-align-sm="right"
+          label-size="sm"
+          description="Leave all unchecked to filter on all data"
+	  class="mb-0"
+	  >
+          <b-form-checkbox-group
+	    v-model="filterOn"
+	    class="mt-1 mb-0">
+            <b-form-checkbox value="from">From</b-form-checkbox>
+            <b-form-checkbox value="recipients">Recipients</b-form-checkbox>
+            <b-form-checkbox value="subject">Subject</b-form-checkbox>
+            <b-form-checkbox value="body">Message body</b-form-checkbox>
+          </b-form-checkbox-group>
+	</b-form-group>
+    </b-col>
+  </b-row>
+  </b-card>
   <b-pagination
     v-model="currentPage"
     :total-rows="totalRows"
     :per-page="perPage"
+    class="m-1"
+    align="center"
     aria-controls="my-table"
     >
   </b-pagination>
+  <p align="center">{{ totalRows }} messages</p>
   <b-table
     id="my-table"
     :items="messagesProvider"
     :fields="fields"
+    :filter="filter"
+    :filterIncludedFields="filterOn"
+    @filtered="onFiltered"
     :per-page="perPage"
     :current-page="currentPage"
     >
@@ -37,7 +87,7 @@ export default {
     },
     data() {
 	return {
-	    perPage: 1,
+	    perPage: 10,
 	    currentPage: 1,
 	    totalRows: 1,
 	    fields: [
@@ -47,12 +97,28 @@ export default {
 		{ key: 'data.recipients', label: 'Recipients' },
 		{ key: 'actions', label: 'Actions' }
 	    ],
+	    filter: null,
+	    filterOn: []
 	}
     },
     methods: {
 	messagesProvider(ctx) {
+	    console.log(ctx)
 	    // Our API uses limit/offset pagination
-	    const params = '?limit=' + ctx.perPage + '&offset=' + ctx.perPage * (ctx.currentPage - 1)
+	    var params = '?limit=' + ctx.perPage + '&offset=' + ctx.perPage * (ctx.currentPage - 1)
+	    // Add search query (if it exists)
+	    if (this.filter) {
+		var filterlist = ['from', 'recipients', 'subject', 'body']
+		if (this.filterOn.length > 0) {
+		    filterlist = this.filterOn
+		}
+
+		filterlist.forEach((filterfield) => {
+		    params += '&' + filterfield + '=' + this.filter
+		});
+	    }
+	    console.log("params: " + params)
+	    console.log("filter on: " + filterlist)
 	    console.log(ctx.perPage)
 	    console.log(ctx.currentPage)
 	    console.log(ctx.perPage * (ctx.currentPage - 1))
@@ -64,6 +130,10 @@ export default {
 		    this.totalRows = data.count
 		    return items || []
 		})
+	},
+	onFiltered(filteredItems) {
+            this.totalRows = filteredItems.length
+            this.currentPage = 1
 	}
     }
 }
