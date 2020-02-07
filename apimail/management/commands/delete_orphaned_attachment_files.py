d98885224d404aea8f6055cc9b6c70fa9d5889f0
J.-S. Caux <J.S.Caux@uva.nl>
2020-02-07 11:28:16 +0100

    Modify logic of delete_orphaned_attachment_file

--- apimail/management/commands/delete_orphaned_attachment_files.py
+++ apimail/management/commands/delete_orphaned_attachment_files.py
@@ -18,22 +18,14 @@ class Command(BaseCommand):
                        for att in sm.attachment_files.all()]
 
         uuids_in_use = set(uuids_in_cm + uuids_in_sm)
-        print(uuids_in_use)
-
         orphaned_att = AttachmentFile.objects.exclude(uuid__in=uuids_in_use)
-
-        print(orphaned_att)
 
         for orphan_att in orphaned_att:
             # We double-check that we're not deleting any used attachment
             # since the 'exclude' logic above is risky
             # (any mistake in uuids_in_use would lead to unwanted deletion)
-            try:
-                ComposedMessage.objects.filter(attachment_files__uuid=orphan_att.uuid).first()
-            except ComposedMessage.DoesNotExist:
-                try:
-                    StoredMessage.objects.filter(attachment_files__uuid=orphan_att.uuid).first()
-                except StoredMessage.DoesNotExist:
+            if not ComposedMessage.objects.filter(attachment_files__uuid=orphan_att.uuid).exists():
+                if not StoredMessage.objects.filter(attachment_files__uuid=orphan_att.uuid).exists():
                     print('Deleting %s' % orphan_att.uuid)
                     if orphan_att.file:
                         if os.path.isfile(orphan_att.file.path):
