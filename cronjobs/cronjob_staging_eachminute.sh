d911dd4c539c9e7ea26945ba9f643054210af8eb
J.-S. Caux <J.S.Caux@uva.nl>
2020-10-22 08:46:41 +0200

    Correct staging cronjob

--- cronjobs/cronjob_staging_eachminute.sh
+++ cronjobs/cronjob_staging_eachminute.sh
@@ -7,6 +7,6 @@ source venv/bin/activate
 
 # Mails waiting in the database
 #python manage.py send_mails
-python manage.py mailgun_get_events --settings=SciPost_v1.settings.staging_do1
+python manage.py mailgun_get_events --nr_minutes=2 --settings=SciPost_v1.settings.staging_do1
 python manage.py mailgun_get_stored_messages --settings=SciPost_v1.settings.staging_do1
 python manage.py mailgun_send_messages --settings=SciPost_v1.settings.staging_do1
