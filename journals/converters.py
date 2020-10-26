36c4bcad7b9406b3f6edca11f565b3a97a75d73b
J.-S. Caux <J.S.Caux@uva.nl>
2020-10-26 21:46:19 +0100

    Add submission_insert field of journals.Journal

--- journals/converters.py
+++ journals/converters.py
@@ -2,13 +2,24 @@ __copyright__ = "Copyright © Stichting SciPost (SciPost Foundation)"
 __license__ = "AGPL v3"
 
 
-from journals.models import Journal
+from django.db.utils import ProgrammingError
+
+def get_journal_doi_label_converter_regex():
+    """
+    Helper function to prevent migrations from crashing.
+    """
+    from journals.models import Journal
+    try:
+        return '|'.join([j.doi_label for j in Journal.objects.all()])
+    except ProgrammingError:
+        return 'SciPostPhys'
 
 
 class JournalDOILabelConverter:
-    regex = '|'.join([j.doi_label for j in Journal.objects.all()])
+    regex = get_journal_doi_label_converter_regex()
 
     def to_python(self, value):
+        from journals.models import Journal
         try:
             return Journal.objects.get(doi_label=value).doi_label
         except Journal.DoesNotExist:
