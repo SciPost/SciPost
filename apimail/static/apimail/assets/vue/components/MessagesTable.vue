158400c787bbf038c60e15b09b2f65984c945703
J.-S. Caux <J.S.Caux@uva.nl>
2020-10-28 14:34:50 +0100

    Remove debug output

--- apimail/static/apimail/assets/vue/components/MessagesTable.vue
+++ apimail/static/apimail/assets/vue/components/MessagesTable.vue
@@ -545,7 +545,6 @@ export default {
     },
     methods: {
 	fetchAccounts () {
-	    console.log("Fetching accounts")
 	    fetch('/mail/api/user_account_accesses')
 		.then(stream => stream.json())
 		.then(data => this.accesses = data.results)
