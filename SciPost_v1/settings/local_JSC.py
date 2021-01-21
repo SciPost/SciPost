c377d4d07d4f2f61bbb28724d83d8b33b368e3f5
J.-S. Caux <J.S.Caux@uva.nl>
2021-01-21 11:52:39 +0100

    Correct typo in email template

--- SciPost_v1/settings/local_JSC.py
+++ SciPost_v1/settings/local_JSC.py
@@ -4,10 +4,10 @@ from .base import *
 DEBUG = True
 
 # Static and media
-STATIC_ROOT = '/Users/jscaux/Sites/SciPost.org/scipost_v1/local_files/static/'
-MEDIA_ROOT = '/Users/jscaux/Sites/SciPost.org/scipost_v1/local_files/media/'
+STATIC_ROOT = '/home/jscaux/SciPost/sites/scipost.org/SciPost/local_files/static/'
+MEDIA_ROOT = '/home/jscaux/SciPost/sites/scipost.org/SciPost/local_files/media/'
 WEBPACK_LOADER['DEFAULT']['BUNDLE_DIR_NAME'] =\
-    '/Users/jscaux/Sites/SciPost.org/scipost_v1/local_files/static/bundles/'
+    '/home/jscaux/SciPost/sites/scipost.org/SciPost/local_files/static/bundles/'
 
 CROSSREF_LOGIN_ID = get_secret("CROSSREF_LOGIN_ID")
 CROSSREF_LOGIN_PASSWORD = get_secret("CROSSREF_LOGIN_PASSWORD")
@@ -18,10 +18,10 @@ MAILCHIMP_API_USER = get_secret("MAILCHIMP_API_USER")
 MAILCHIMP_API_KEY = get_secret("MAILCHIMP_API_KEY")
 
 # Logging
-LOGGING['handlers']['scipost_file_arxiv']['filename'] = '/Users/jscaux/Sites/SciPost.org/scipost_v1/local_files/logs/arxiv.log'
-LOGGING['handlers']['scipost_file_doi']['filename'] = '/Users/jscaux/Sites/SciPost.org/scipost_v1/local_files/logs/doi.log'
-LOGGING['handlers']['api_file']['filename'] = '/Users/jscaux/Sites/SciPost.org/scipost_v1/local_files/logs/api.log'
-LOGGING['handlers']['oauth_file']['filename'] = '/Users/jscaux/Sites/SciPost.org/scipost_v1/local_files/logs/oauth.log'
+LOGGING['handlers']['scipost_file_arxiv']['filename'] = '/home/jscaux/SciPost/sites/scipost.org/SciPost/local_files/logs/arxiv.log'
+LOGGING['handlers']['scipost_file_doi']['filename'] = '/home/jscaux/SciPost/sites/scipost.org/SciPost/local_files/logs/doi.log'
+LOGGING['handlers']['api_file']['filename'] = '/home/jscaux/SciPost/sites/scipost.org/SciPost/local_files/logs/api.log'
+LOGGING['handlers']['oauth_file']['filename'] = '/home/jscaux/SciPost/sites/scipost.org/SciPost/local_files/logs/oauth.log'
 
 CROSSREF_DEPOSIT_EMAIL = 'jscaux@scipost.org'
 
