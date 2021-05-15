45617b3e1738c180e35f8ee30ed2a951c541f806
J.-S. Caux <J.S.Caux@uva.nl>
2021-05-15 11:57:39 +0200

    Mark ref inv as unsent; resend button. Resolves #1

--- submissions/models/referee_invitation.py
+++ submissions/models/referee_invitation.py
@@ -23,12 +23,22 @@ class RefereeInvitation(SubmissionRelatedObjectMixin, models.Model):
     The instance will register the response to the invitation and
     the current status of the refereeing duty if the invitation has been accepted.
     """
-    profile = models.ForeignKey('profiles.Profile', on_delete=models.SET_NULL,
-                                blank=True, null=True)
-    submission = models.ForeignKey('submissions.Submission', on_delete=models.CASCADE,
-                                   related_name='referee_invitations')
-    referee = models.ForeignKey('scipost.Contributor', related_name='referee_invitations',
-                                blank=True, null=True, on_delete=models.CASCADE)
+    profile = models.ForeignKey(
+        'profiles.Profile',
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
+    submission = models.ForeignKey(
+        'submissions.Submission',
+        on_delete=models.CASCADE,
+        related_name='referee_invitations'
+    )
+    referee = models.ForeignKey(
+        'scipost.Contributor',
+        related_name='referee_invitations',
+        blank=True, null=True,
+        on_delete=models.CASCADE
+    )
     title = models.CharField(max_length=4, choices=TITLE_CHOICES)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
@@ -36,16 +46,22 @@ class RefereeInvitation(SubmissionRelatedObjectMixin, models.Model):
 
     # if Contributor not found, person is invited to register
     invitation_key = models.CharField(max_length=40, blank=True)
-    date_invited = models.DateTimeField(default=timezone.now)
-    invited_by = models.ForeignKey('scipost.Contributor', related_name='referee_invited_by',
-                                   blank=True, null=True, on_delete=models.CASCADE)
+    date_invited = models.DateTimeField(blank=True, null=True)
+    invited_by = models.ForeignKey(
+        'scipost.Contributor',
+        related_name='referee_invited_by',
+        blank=True, null=True, on_delete=models.CASCADE
+    )
     auto_reminders_allowed = models.BooleanField(default=True)
     nr_reminders = models.PositiveSmallIntegerField(default=0)
     date_last_reminded = models.DateTimeField(blank=True, null=True)
     accepted = models.NullBooleanField(choices=ASSIGNMENT_NULLBOOL, default=None)
     date_responded = models.DateTimeField(blank=True, null=True)
-    refusal_reason = models.CharField(max_length=3, choices=ASSIGNMENT_REFUSAL_REASONS,
-                                      blank=True, null=True)
+    refusal_reason = models.CharField(
+        max_length=3,
+        choices=ASSIGNMENT_REFUSAL_REASONS,
+        blank=True, null=True
+    )
     fulfilled = models.BooleanField(default=False)  # True if a Report has been submitted
     cancelled = models.BooleanField(default=False)  # True if EIC has deactivated invitation
 
@@ -56,9 +72,14 @@ class RefereeInvitation(SubmissionRelatedObjectMixin, models.Model):
 
     def __str__(self):
         """Summarize the RefereeInvitation's basic information."""
-        return (self.first_name + ' ' + self.last_name + ' to referee ' +
-                self.submission.title[:30] + ' by ' + self.submission.author_list[:30] +
-                ', invited on ' + self.date_invited.strftime('%Y-%m-%d'))
+        value = (self.first_name + ' ' + self.last_name + ' to referee ' +
+                self.submission.title[:30] + ' by ' +
+                self.submission.author_list[:30])
+        if self.date_invited:
+            value += ', invited on ' + self.date_invited.strftime('%Y-%m-%d')
+        else:
+            value += ', NO EMAIL SENT YET'
+        return value
 
     def get_absolute_url(self):
         """Return url of the invitation's processing page."""
@@ -82,6 +103,13 @@ class RefereeInvitation(SubmissionRelatedObjectMixin, models.Model):
         return self.submission.reports.filter(author=self.referee).last()
 
     @property
+    def needs_sending(self):
+        """Check if the invitation has been emailed."""
+        if not self.date_invited:
+            return True
+        return False
+
+    @property
     def needs_response(self):
         """Check if invitation has no response in more than three days."""
         if not self.cancelled and self.accepted is None:
@@ -90,7 +118,10 @@ class RefereeInvitation(SubmissionRelatedObjectMixin, models.Model):
                 return timezone.now() - self.date_last_reminded > datetime.timedelta(days=3)
 
             # No reponse in over three days since original invite
-            return timezone.now() - self.date_invited > datetime.timedelta(days=3)
+            if not self.date_invited:
+                return True
+            else:
+                return timezone.now() - self.date_invited > datetime.timedelta(days=3)
 
         return False
 
@@ -113,7 +144,9 @@ class RefereeInvitation(SubmissionRelatedObjectMixin, models.Model):
     @property
     def needs_attention(self):
         """Check if invitation needs attention by the editor."""
-        return self.needs_response or self.needs_fulfillment_reminder
+        return (self.needs_sending or
+                self.needs_response or
+                self.needs_fulfillment_reminder)
 
     @property
     def get_status_display(self):
