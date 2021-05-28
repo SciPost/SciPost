f7d449a95f207aadef6434ba428ea51afa0dd335
J.-S. Caux <J.S.Caux@uva.nl>
2021-05-28 11:12:10 +0200

    Repair pool-related functionalities

--- scipost_django/scipost/static/scipost/button-toggle-modal-load.js
+++ scipost_django/scipost/static/scipost/button-toggle-modal-load.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('button[data-bs-toggle="modal"]').on('click',function(){
-	var dataURL = $(this).attr('data-href');
-	target_body = $(this).attr('data-target-body');
+	var dataURL = $(this).attr('data-bs-href');
+	target_body = $(this).attr('data-bs-target-body');
 	$(target_body).load(dataURL);
     });
 });
