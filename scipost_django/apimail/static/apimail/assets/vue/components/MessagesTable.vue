98ef22d0355302161b5559ecdeb240ce80937070
J.-S. Caux <J.S.Caux@uva.nl>
2021-05-29 16:08:57 +0200

    .ml-, .mr- to .ms-, .me-

--- scipost_django/apimail/static/apimail/assets/vue/components/MessagesTable.vue
+++ scipost_django/apimail/static/apimail/assets/vue/components/MessagesTable.vue
@@ -456,7 +456,7 @@
 	>
 	<template v-slot:title>
 	  {{ tabbedMessages.length - index }}
-	  <b-button v-if="!threadOf" size="sm" variant="light" class="float-right ml-2 p-0" @click="closeTab(message.uuid)">
+	  <b-button v-if="!threadOf" size="sm" variant="light" class="float-right ms-2 p-0" @click="closeTab(message.uuid)">
 	    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-x" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
 	      <path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
 	    </svg>
