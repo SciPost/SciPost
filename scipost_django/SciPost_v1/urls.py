63c5e3a769c910e8cc8b2440a341f189ba388866
J.-S. Caux <J.S.Caux@uva.nl>
2021-07-16 17:58:34 +0200

    Some url() -> path()

--- scipost_django/SciPost_v1/urls.py
+++ scipost_django/SciPost_v1/urls.py
@@ -4,12 +4,9 @@ __license__ = "AGPL v3"
 
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
-from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, register_converter
-
-# from rest_framework import routers
+from django.urls import include, path, register_converter
 
 from journals.converters import JournalDOILabelConverter
 from scipost import views as scipost_views
@@ -24,15 +21,31 @@ admin.site.login = login_required(admin.site.login)
 # Base URLs
 urlpatterns = [
 
-    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
-    url(r'^sitemap.xml$', scipost_views.sitemap_xml, name='sitemap_xml'),
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/', admin.site.urls),
+    path(
+        'o/',
+        include('oauth2_provider.urls', namespace='oauth2_provider')
+    ),
+    path(
+        'sitemap.xml',
+        scipost_views.sitemap_xml,
+        name='sitemap_xml'
+    ),
+    path(
+        'admin/doc/',
+        include('django.contrib.admindocs.urls')
+    ),
+    path(
+        'admin/',
+        admin.site.urls
+    ),
     path(
         'affiliates/',
         include('affiliates.urls', namespace='affiliates')
     ),
-    url(r'^api/', include('api.urls', namespace='api')),
+    path(
+        'api/',
+        include('api.urls', namespace='api')
+    ),
     path(
         'mail/',
         include('apimail.urls', namespace='apimail')
@@ -45,44 +58,136 @@ urlpatterns = [
         '<journal_doi_label:doi_label>/',
         include('journals.urls.journal', namespace="journal")
     ),
-    url(r'^', include('scipost.urls', namespace="scipost")),
-    url(r'^careers/', include('careers.urls', namespace="careers")),
-    url(r'^colleges/', include('colleges.urls', namespace="colleges")),
-    url(r'^commentaries/', include('commentaries.urls', namespace="commentaries")),
-    url(r'^commentary/', include('commentaries.urls', namespace="_commentaries")),
-    url(r'^comments/', include('comments.urls', namespace="comments")),
-    url(r'^forums/', include('forums.urls', namespace="forums")),
-    url(r'^funders/', include('funders.urls', namespace="funders")),
-    url(r'^finances/', include('finances.urls', namespace="finances")),
-    url(r'^guides/', include('guides.urls', namespace="guides")),
-    url(r'^helpdesk/', include('helpdesk.urls', namespace="helpdesk")),
-    url(r'^invitations/', include('invitations.urls', namespace="invitations")),
-    url(r'^journals/', include('journals.urls.general', namespace="journals")),
-    url(r'^mailing_list/', include('mailing_lists.urls', namespace="mailing_lists")),
-    url(r'^markup/', include('markup.urls', namespace='markup')),
-    url(r'^submissions/', include('submissions.urls', namespace="submissions")),
-    url(r'^submission/', include('submissions.urls', namespace="_submissions")),
-    url(r'^theses/', include('theses.urls', namespace="theses")),
-    url(r'^thesis/', include('theses.urls', namespace="_theses")),
-    url(r'^mails/', include('mails.urls', namespace="mails")),
-    url(r'^news/', include('news.urls', namespace="news")),
-    url(r'^ontology/', include('ontology.urls', namespace="ontology")),
-    url(r'^organizations/', include('organizations.urls', namespace="organizations")),
-    url(r'^petitions/', include('petitions.urls', namespace="petitions")),
-    url(r'^preprints/', include('preprints.urls', namespace="preprints")),
-    url(r'^proceedings/', include('proceedings.urls', namespace="proceedings")),
-    url(r'^production/', include('production.urls', namespace="production")),
-    url(r'^profiles/', include('profiles.urls', namespace="profiles")),
-    url(r'^security/', include('security.urls', namespace="security")),
-    url(r'^series/', include('series.urls', namespace="series")),
-    url(r'^sponsors/', include('sponsors.urls', namespace="sponsors")),
-    url(r'^stats/', include('stats.urls', namespace="stats")),
-    # Deprecated, keep temporarily for historical reasons
-    url(r'^partners/', OrganizationListView.as_view(), name='partners'),
-    url(r'^supporting_partners/', OrganizationListView.as_view(), name='partners'),
+    path(
+        '',
+        include('scipost.urls', namespace="scipost")
+    ),
+    path(
+        'careers/',
+        include('careers.urls', namespace="careers")
+    ),
+    path(
+        'colleges/',
+        include('colleges.urls', namespace="colleges")
+    ),
+    path(
+        'commentaries/',
+        include('commentaries.urls', namespace="commentaries")
+    ),
+    path(
+        'commentary/',
+        include('commentaries.urls', namespace="_commentaries")
+    ),
+    path(
+        'comments/',
+        include('comments.urls', namespace="comments")
+    ),
+    path(
+        'forums/',
+        include('forums.urls', namespace="forums")
+    ),
+    path(
+        'funders/',
+        include('funders.urls', namespace="funders")
+    ),
+    path(
+        'finances/',
+        include('finances.urls', namespace="finances")
+    ),
+    path(
+        'guides/',
+        include('guides.urls', namespace="guides")
+    ),
+    path(
+        'helpdesk/',
+        include('helpdesk.urls', namespace="helpdesk")
+    ),
+    path(
+        'invitations/',
+        include('invitations.urls', namespace="invitations")
+    ),
+    path(
+        'journals/',
+        include('journals.urls.general', namespace="journals")
+    ),
+    path(
+        'mailing_list/',
+        include('mailing_lists.urls', namespace="mailing_lists")
+    ),
+    path(
+        'markup/',
+        include('markup.urls', namespace='markup')
+    ),
+    path(
+        'submissions/',
+        include('submissions.urls', namespace="submissions")
+    ),
+    path(
+        'submission/',
+        include('submissions.urls', namespace="_submissions")
+    ),
+    path(
+        'theses/',
+        include('theses.urls', namespace="theses")
+    ),
+    path(
+        'thesis/',
+        include('theses.urls', namespace="_theses")
+    ),
+    path(
+        'mails/',
+        include('mails.urls', namespace="mails")
+    ),
+    path(
+        'news/',
+        include('news.urls', namespace="news")
+    ),
+    path(
+        'ontology/',
+        include('ontology.urls', namespace="ontology")
+    ),
+    path(
+        'organizations/',
+        include('organizations.urls', namespace="organizations")
+    ),
+    path(
+        'petitions/',
+        include('petitions.urls', namespace="petitions")
+    ),
+    path(
+        'preprints/',
+        include('preprints.urls', namespace="preprints")
+    ),
+    path(
+        'proceedings/',
+        include('proceedings.urls', namespace="proceedings")
+    ),
+    path(
+        'production/',
+        include('production.urls', namespace="production")
+    ),
+    path(
+        'profiles/',
+        include('profiles.urls', namespace="profiles")
+    ),
+    path(
+        'security/',
+        include('security.urls', namespace="security")
+    ),
+    path(
+        'series/', include('series.urls', namespace="series")
+    ),
+    path(
+        'sponsors/',
+        include('sponsors.urls', namespace="sponsors")
+    ),
+    path(
+        'stats/',
+        include('stats.urls', namespace="stats")
+    ),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns += [url(r'^__debug__/', include(debug_toolbar.urls))]
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
