7a4fed332f1533e1a294a20ff46778e19774ff39
Jean-Sébastien Caux <git@jscaux.org>
2021-11-03 14:13:24 +0100

    Add voting info to `_hx_submission_li`

--- scipost_django/scipost/static/scipost/toggle-proceedings.js
+++ scipost_django/scipost/static/scipost/toggle-proceedings.js
@@ -1,6 +1,6 @@
 /* If Proceedings is chosen as Journal, display Proceedings selector */
 
-/* for publications tab */
+/* for portal publications tab */
 if (document.getElementById("id_journal")) {
     document.getElementById("id_journal").addEventListener("change", () => {
 	var e = document.getElementById("id_journal")
@@ -13,7 +13,7 @@ if (document.getElementById("id_journal")) {
     })
 }
 
-/* for submissions tab */
+/* for portal/pool submissions tab */
 if (document.getElementById("id_submitted_to")) {
     document.getElementById("id_submitted_to").addEventListener("change", () => {
 	var e = document.getElementById("id_submitted_to")
@@ -25,3 +25,17 @@ if (document.getElementById("id_submitted_to")) {
 	}
     })
 }
+
+/* For pool. If user opts for viewing submissions for which they are EIC, toggle
+   editor_in_charge selector and set status to 'all' */
+document.getElementsByName("search_set").forEach(e => {
+    e.addEventListener("change", () => {
+	if (e.checked && e.value == 'eic') {
+	    document.getElementById("id_editor_in_charge").value = null
+	    document.getElementById("col_eic").style.display = 'none'
+	    document.getElementById("id_status").value = 'all'
+	} else {
+	    document.getElementById("col_eic").style.display = 'block'
+	}
+    })
+})
