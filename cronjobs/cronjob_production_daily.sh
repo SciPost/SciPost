b671c5a72065ac533e25ddff28fbc9f2daff442e
Jean-Sébastien Caux <git@jscaux.org>
2022-02-23 11:11:32 +0100

    Add update new computed fields to daily cronjobs

--- cronjobs/cronjob_production_daily.sh
+++ cronjobs/cronjob_production_daily.sh
@@ -5,4 +5,10 @@
 cd /home/scipost/SciPost/scipost_django
 source ../venv-3.8.5/bin/activate
 
+python manage.py organization_update_cf_associated_publication_ids --settings=SciPost_v1.settings.production_do1
+
 python manage.py organization_update_cf_nr_associated_publications --settings=SciPost_v1.settings.production_do1
+
+python manage.py organization_update_cf_nr_expenditures_for_publication --settings=SciPost_v1.settings.production_do1
+
+python manage.py organization_update_cf_balance_info --settings=SciPost_v1.settings.production_do1
