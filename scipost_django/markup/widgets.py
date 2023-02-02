cc545f1d917c92b17d7723028ff0a54eb0f3765a
Jean-Sébastien Caux <git@jscaux.org>
2023-02-02 08:46:48 +0100

    Add markup TextareaWithPreview widget

---
+++ scipost_django/markup/widgets.py
@@ -0,0 +1,19 @@
+__copyright__ = "Copyright © Stichting SciPost (SciPost Foundation)"
+__license__ = "AGPL v3"
+
+
+from django.forms.widgets import Textarea
+
+from .utils import process_markup
+
+
+class TextareaWithPreview(Textarea):
+    template_name = 'markup/forms/widgets/textarea_with_preview.html'
+
+    def get_context(self, name, value, attrs):
+        context = super().get_context(name, value, attrs)
+        context["value_processed"] = process_markup(
+            context["widget"]["value"],
+            include_errors=True,
+        )
+        return context
