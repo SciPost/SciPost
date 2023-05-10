ef4fc3d37cb315059fc0ed6316391189913a30da
George Katsikas <giorgakis.katsikas@gmail.com>
2023-05-10 11:25:12 +0200

    add vet_superusers command

---
+++ scipost_django/scipost/management/commands/vet_superusers.py
@@ -0,0 +1,18 @@
+__copyright__ = "Copyright © Stichting SciPost (SciPost Foundation)"
+__license__ = "AGPL v3"
+
+
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User, Group
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        superusers = User.objects.filter(is_superuser=True)
+        admin_group = Group.objects.get(name='SciPost Administrators')
+
+        for superuser in superusers:
+            superuser.groups.add(admin_group)
+            superuser.save()
+
+        self.stdout.write(self.style.SUCCESS(f"Successfully vetted {len(superusers)} superusers."))
