1a4a507506e959922c2768cecd119953a51d18af
George Katsikas <giorgakis.katsikas@gmail.com>
2023-05-12 15:42:27 +0200

    add proofs repo object and tests

--- scipost_django/production/models.py
+++ scipost_django/production/models.py
@@ -6,8 +6,14 @@ from django.db import models
 from django.contrib.contenttypes.fields import GenericRelation
 from django.urls import reverse
 from django.contrib.auth.models import User
+from profiles.models import Profile
 from django.utils import timezone
 from django.utils.functional import cached_property
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.db.models import Value
+from django.db.models.functions import Concat
+
 
 from .constants import (
     PRODUCTION_STREAM_STATUS,
@@ -18,6 +24,8 @@ from .constants import (
     PRODUCTION_STREAM_COMPLETED,
     PROOFS_STATUSES,
     PROOFS_UPLOADED,
+    PROOFS_REPO_STATUSES,
+    PROOFS_REPO_UNINITIALIZED,
 )
 from .managers import (
     ProductionStreamQuerySet,
@@ -126,17 +134,11 @@ class ProductionStream(models.Model):
 
 
 class ProductionEvent(models.Model):
-    stream = models.ForeignKey(
-        ProductionStream, on_delete=models.CASCADE, related_name="events"
-    )
-    event = models.CharField(
-        max_length=64, choices=PRODUCTION_EVENTS, default=EVENT_MESSAGE
-    )
+    stream = models.ForeignKey(ProductionStream, on_delete=models.CASCADE, related_name="events")
+    event = models.CharField(max_length=64, choices=PRODUCTION_EVENTS, default=EVENT_MESSAGE)
     comments = models.TextField(blank=True, null=True)
     noted_on = models.DateTimeField(default=timezone.now)
-    noted_by = models.ForeignKey(
-        "production.ProductionUser", on_delete=models.CASCADE, related_name="events"
-    )
+    noted_by = models.ForeignKey("production.ProductionUser", on_delete=models.CASCADE, related_name="events")
     noted_to = models.ForeignKey(
         "production.ProductionUser",
         on_delete=models.CASCADE,
@@ -159,10 +161,7 @@ class ProductionEvent(models.Model):
 
     @cached_property
     def editable(self):
-        return (
-            self.event in [EVENT_MESSAGE, EVENT_HOUR_REGISTRATION]
-            and not self.stream.completed
-        )
+        return self.event in [EVENT_MESSAGE, EVENT_HOUR_REGISTRATION] and not self.stream.completed
 
 
 def production_event_upload_location(instance, filename):
@@ -185,9 +184,7 @@ class ProductionEventAttachment(models.Model):
         on_delete=models.CASCADE,
         related_name="attachments",
     )
-    attachment = models.FileField(
-        upload_to=production_event_upload_location, storage=SecureFileStorage()
-    )
+    attachment = models.FileField(upload_to=production_event_upload_location, storage=SecureFileStorage())
 
     def get_absolute_url(self):
         return reverse(
@@ -213,20 +210,12 @@ class Proofs(models.Model):
     Proofs are directly related to a ProductionStream and Submission in SciPost.
     """
 
-    attachment = models.FileField(
-        upload_to=proofs_upload_location, storage=SecureFileStorage()
-    )
+    attachment = models.FileField(upload_to=proofs_upload_location, storage=SecureFileStorage())
     version = models.PositiveSmallIntegerField(default=0)
-    stream = models.ForeignKey(
-        "production.ProductionStream", on_delete=models.CASCADE, related_name="proofs"
-    )
-    uploaded_by = models.ForeignKey(
-        "production.ProductionUser", on_delete=models.CASCADE, related_name="+"
-    )
+    stream = models.ForeignKey("production.ProductionStream", on_delete=models.CASCADE, related_name="proofs")
+    uploaded_by = models.ForeignKey("production.ProductionUser", on_delete=models.CASCADE, related_name="+")
     created = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(
-        max_length=16, choices=PROOFS_STATUSES, default=PROOFS_UPLOADED
-    )
+    status = models.CharField(max_length=16, choices=PROOFS_STATUSES, default=PROOFS_UPLOADED)
     accessible_for_authors = models.BooleanField(default=False)
 
     objects = ProofsQuerySet.as_manager()
@@ -239,9 +228,7 @@ class Proofs(models.Model):
         return reverse("production:proofs_pdf", kwargs={"slug": self.slug})
 
     def __str__(self):
-        return "Proofs {version} for Stream {stream}".format(
-            version=self.version, stream=self.stream.submission.title
-        )
+        return "Proofs {version} for Stream {stream}".format(version=self.version, stream=self.stream.submission.title)
 
     def save(self, *args, **kwargs):
         # Control Report count per Submission.
@@ -252,3 +239,96 @@ class Proofs(models.Model):
     @property
     def slug(self):
         return proofs_id_to_slug(self.id)
+
+
+class ProofsRepository(models.Model):
+    """
+    ProofsRepository is a GitLab repository of Proofs for a Submission.
+    """
+
+    stream = models.OneToOneField(
+        ProductionStream,
+        on_delete=models.CASCADE,
+        related_name="proofs_repository",
+    )
+    status = models.CharField(
+        max_length=32,
+        choices=PROOFS_REPO_STATUSES,
+        default=PROOFS_REPO_UNINITIALIZED,
+    )
+
+    @property
+    def name(self) -> str:
+        """
+        Return the name of the repository in the form of "id_lastname".
+        """
+        # Get the last name of the first author by getting the first author string from the submission
+        first_author_str = self.stream.submission.authors_as_list[0]
+        first_author_profile = (
+            Profile.objects.annotate(
+                full_name=Concat("first_name", Value(" "), "last_name")
+            )
+            .filter(full_name=first_author_str)
+            .first()
+        )
+        if first_author_profile is None:
+            first_author_last_name = first_author_str.split(" ")[-1]
+        else:
+            first_author_last_name = first_author_profile.last_name
+            # Keep only the last of the last names
+            first_author_last_name = first_author_last_name.split(" ")[-1]
+
+        return "{preprint_id}_{last_name}".format(
+            preprint_id=self.stream.submission.preprint.identifier_w_vn_nr,
+            last_name=first_author_last_name,
+        )
+
+    @property
+    def journal_path_abbrev(self) -> str:
+        # The DOI label is used to determine the path of the repository and template
+        journal_abbrev = (
+            self.stream.submission.editorial_decision.for_journal.doi_label
+        )
+        return journal_abbrev
+
+    @property
+    def git_path(self) -> str:
+        # Get creation date of the stream
+        # Warning: The month grouping of streams was done using the tasked date,
+        # but should now instead be the creation (opened) date.
+        creation_year, creation_month = self.stream.opened.strftime(
+            "%Y-%m"
+        ).split("-")
+
+        return "/Proofs/{journal}/{year}/{month}/{repo_name}".format(
+            journal=self.journal_path_abbrev,
+            year=creation_year,
+            month=creation_month,
+            repo_name=self.name,
+        )
+
+    @property
+    def template_path(self) -> str:
+        return "Templates/{journal}".format(journal=self.journal_path_abbrev)
+
+    def __str__(self) -> str:
+        return f"Proofs repo for {self.stream}"
+
+    class Meta:
+        verbose_name_plural = "proofs repositories"
+
+
+@receiver(post_save, sender=ProductionStream)
+def production_stream_create_proofs_repo(sender, instance, created, **kwargs):
+    """
+    If a ProductionStream instance is created, a Proofs Repository instance is created
+    and linked to it.
+    """
+    if created:
+        ProofsRepository.objects.create(
+            stream=instance,
+            status=PROOFS_REPO_UNINITIALIZED,
+        )
+
+
+post_save.connect(production_stream_create_proofs_repo, sender=ProductionStream)
