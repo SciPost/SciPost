d8420c9f11a7832454687d113ce124ed6ae45b98
George Katsikas <giorgakis.katsikas@gmail.com>
2024-03-15 18:29:09 +0100

    add notes list view

--- scipost_django/pins/urls.py
+++ scipost_django/pins/urls.py
@@ -3,14 +3,26 @@ __license__ = "AGPL v3"
 
 app_name = "pins"
 
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
 urlpatterns = [
     path(
-        "_hx_create/<int:regarding_content_type>/<int:regarding_object_id>",
-        views._hx_note_create_form,
-        name="hx_note_create_form",
+        "<int:regarding_content_type>/<int:regarding_object_id>",
+        include(
+            [
+                path(
+                    "_hx_create",
+                    views._hx_note_create_form,
+                    name="_hx_note_create_form",
+                ),
+                path(
+                    "_hx_list",
+                    views._hx_notes_list,
+                    name="_hx_notes_list",
+                ),
+            ]
+        ),
     ),
 ]
--- scipost_django/pins/views.py
+++ scipost_django/pins/views.py
@@ -6,6 +6,7 @@ from django.template.response import TemplateResponse
 
 from scipost.permissions import HTMXResponse
 
+from .models import Note
 from .forms import NoteForm
 
 
@@ -28,3 +29,21 @@ def _hx_note_create_form(request, regarding_content_type, regarding_object_id):
     context = {"form": form}
 
     return TemplateResponse(request, "pins/_hx_note_create_form.html", context)
+
+
+def _hx_notes_list(request, regarding_content_type, regarding_object_id):
+    regarding_content_type = ContentType.objects.get_for_id(regarding_content_type)
+    object = regarding_content_type.get_object_for_this_type(id=regarding_object_id)
+    notes = Note.objects.filter(
+        regarding_content_type=regarding_content_type,
+        regarding_object_id=regarding_object_id,
+    )
+
+    # TODO: Filter to the notes that the user has permission to see
+    # ...
+
+    context = {
+        "object": object,
+        "notes": notes,
+    }
+    return TemplateResponse(request, "pins/_hx_notes_list.html", context)
