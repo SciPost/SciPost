eb7c451bda66f296b256b1ae7629fb35d76846d2
George Katsikas <giorgakis.katsikas@gmail.com>
2024-03-20 12:15:45 +0100

    add factories for all theses models

--- scipost_django/theses/factories.py
+++ scipost_django/theses/factories.py
@@ -9,9 +9,11 @@ from ontology.models import AcademicField, Specialty
 from scipost.constants import SCIPOST_APPROACHES
 from scipost.models import Contributor
 
-from .models import ThesisLink
+from .models import *
 from .forms import VetThesisLinkForm
 from .constants import THESIS_TYPES
+
+from common.faker import LazyRandEnum, fake
 
 
 class BaseThesisLinkFactory(factory.django.DjangoModelFactory):
@@ -23,33 +25,16 @@ class BaseThesisLinkFactory(factory.django.DjangoModelFactory):
     vetted_by = factory.SubFactory("scipost.factories.ContributorFactory")
     vetted = True
 
-    type = factory.Iterator(THESIS_TYPES, getter=lambda c: c[0])
+    type = LazyRandEnum(THESIS_TYPES)
     acad_field = factory.SubFactory("ontology.factories.AcademicFieldFactory")
-    approaches = factory.Iterator(
-        SCIPOST_APPROACHES,
-        getter=lambda c: [
-            c[0],
-        ],
-    )
+    approaches = LazyRandEnum(SCIPOST_APPROACHES, repeat=2)
     title = factory.Faker("sentence")
     pub_link = factory.Faker("uri")
     author = factory.Faker("name")
     supervisor = factory.Faker("name")
     institution = factory.Faker("company")
-    defense_date = factory.Faker("date_this_decade")
+    defense_date = fake.aware.date_this_decade()
     abstract = factory.Faker("paragraph")
-
-    @classmethod
-    def create(cls, **kwargs):
-        if AcademicField.objects.count() < 5:
-            from ontology.factories import AcademicFieldactory
-
-            AcademicFieldFactory.create_batch(5)
-        if Specialty.objects.count() < 5:
-            from ontology.factories import SpecialtyFactory
-
-            SpecialtyFactory.create_batch(5)
-        return super().create(**kwargs)
 
     @factory.post_generation
     def add_specialties(self, create, extracted, **kwargs):
@@ -66,9 +51,6 @@ class BaseThesisLinkFactory(factory.django.DjangoModelFactory):
             # A list of groups were passed in, use them
             for contributor in extracted:
                 self.author_as_cont.add(contributor)
-        elif factory.Faker("boolean"):
-            contributor = Contributor.objects.order_by("?").first()
-            self.author_as_cont.add(contributor)
 
 
 class ThesisLinkFactory(BaseThesisLinkFactory):
