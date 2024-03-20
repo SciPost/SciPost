d94c0eccb6371e83ef48c71eece75a2350b95d65
George Katsikas <giorgakis.katsikas@gmail.com>
2024-03-20 12:16:29 +0100

    add scaffold factories for all pins models

---
+++ scipost_django/pins/tests/test_factories.py
@@ -0,0 +1,17 @@
+__copyright__ = "Copyright © Stichting SciPost (SciPost Foundation)"
+__license__ = "AGPL v3"
+
+from django.test import TestCase
+from ..factories import *
+
+
+# class TestSubsidyNoteFactory(TestCase):
+#     def test_can_create_subsidy_notes(self):
+#         subsidy_note = SubsidyNoteFactory()
+#         self.assertIsNotNone(subsidy_note)
+
+
+# class TestPinFactory(TestCase):
+#     def test_can_create_pins(self):
+#         pin = PinFactory()
+#         self.assertIsNotNone(pin)
