adaa107f2f01c0b83e505a3590dd8a8fffabaf18
George Katsikas <giorgakis.katsikas@gmail.com>
2024-04-02 09:36:13 +0200

    improve notes admin page

--- scipost_django/pins/admin.py
+++ scipost_django/pins/admin.py
@@ -2,13 +2,22 @@ __copyright__ = "Copyright © Stichting SciPost (SciPost Foundation)"
 __license__ = "AGPL v3"
 
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 
 from pins.models import Note
 
 
 @admin.register(Note)
 class NoteAdmin(admin.ModelAdmin):
-    list_display = ("id", "regarding__object", "title", "author", "created", "modified")
+    list_display = (
+        "id",
+        "regarding_object_links",
+        "title",
+        "author",
+        "created",
+        "modified",
+    )
     list_filter = ("created", "modified", "visibility", "regarding_content_type")
     search_fields = ("title", "author__username")
     date_hierarchy = "created"
@@ -25,5 +34,14 @@ class NoteAdmin(admin.ModelAdmin):
     )
     autocomplete_fields = ("author",)
 
-    def regarding__object(self, obj):
-        return str(obj.regarding)
+    def regarding_object_links(self, obj):
+        content_type = obj.regarding_content_type
+        model = content_type.model_class()
+        regarding = model.objects.get(pk=obj.regarding_object_id)
+        admin_url = f"admin:{content_type.app_label}_{content_type.model}_change"
+        return format_html(
+            "<div style='display: flex; justify-content:space-between;>"
+            f'<a href="{regarding.get_absolute_url()}">{regarding}</a>'
+            f'<a href="{reverse(admin_url, args=[obj.regarding_object_id])}">[admin]</a>'
+            "</div>"
+        )
