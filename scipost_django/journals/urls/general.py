98b9ab6ad72df71812dea4016984fa2d68ebf116
KittenPirate <friesbi7porto@gmail.com>
2024-06-15 21:55:24 +0200

    Remove deprecated code

--- scipost_django/journals/urls/general.py
+++ scipost_django/journals/urls/general.py
@@ -114,21 +114,6 @@ urlpatterns = [
         name="author_affiliations",
     ),
     path(
-        "admin/<publication_doi_label:doi_label>/authoraffiliations/<int:pk>/add/",
-        journals_views.add_affiliation,
-        name="author_affiliation_update",
-    ),
-    path(
-        "admin/<publication_doi_label:doi_label>/authoraffiliations/<int:pk>/add/<int:organization_id>/",
-        journals_views.add_affiliation_id,
-        name="author_affiliation_add_id",
-    ),
-    path(
-        "admin/<publication_doi_label:doi_label>/authoraffiliations/<int:pk>/remove/<int:organization_id>/",
-        journals_views.remove_affiliation,
-        name="author_affiliation_remove",
-    ),
-    path(
         "admin/<publication_doi_label:doi_label>/citation_list_metadata",
         journals_views.CitationUpdateView.as_view(),
         name="create_citation_list_metadata",
