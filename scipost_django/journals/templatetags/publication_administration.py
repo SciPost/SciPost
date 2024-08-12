523a25fbc2fcb52f2600ede69ad9e192bb096bbd
SwoopDoable <swoop-doable.0v@icloud.com>
2024-08-12 17:16:46 +0200

    Add common and organization filters

--- scipost_django/journals/templatetags/publication_administration.py
+++ scipost_django/journals/templatetags/publication_administration.py
@@ -4,33 +4,23 @@ __license__ = "AGPL v3"
 
 from django import template
 
+from journals.models.publication import Publication
+
 register = template.Library()
 
 
 @register.filter
-def has_all_author_relations(publication):
+def has_all_author_relations(publication: Publication) -> bool:
     """
     Check if all authors are added to the Publication object, just by counting.
     """
-    return len(publication.author_list.split(",")) == publication.authors.count()
+    submission_string_authors = publication.author_list.split(",")
+    associated_authors = publication.authors.filter(profile__isnull=False) # exclude temp authors without profiles
 
-
-@register.filter
-def authors_in_right_order(publication):
-    """
-    Checks if all author orderings correspond to those in author list.
-    """
-    if not has_all_author_relations(publication):
-        return False
-    list_of_authors = publication.author_list.split(",")
-    for author in publication.authors.all():
-        if author.last_name and author.last_name not in list_of_authors[author.order - 1]:
-            return False
-    return True
-
+    return len(submission_string_authors) == associated_authors.count()
 
 @register.filter
-def author_affiliations_complete(publication):
+def author_affiliations_complete(publication: Publication) -> bool:
     """
     Checks if each author has a non-empty affiliations field.
     """
