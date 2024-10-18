65da893c8558f830b0c30cc4ebcae75a1286af03
George Katsikas <giorgakis.katsikas@gmail.com>
2024-10-18 12:42:03 +0200

    add last sponsorship year statistics for finadmin

--- scipost_django/sponsors/views.py
+++ scipost_django/sponsors/views.py
@@ -3,8 +3,11 @@ __license__ = "AGPL v3"
 
 
 import datetime
+from django.db.models import F, Q, Case, Count, OuterRef, Subquery, Value, When
+from django.db.models.functions import Extract
 from django.shortcuts import render
 
+from finances.models.subsidy import Subsidy
 from organizations.models import Organization
 
 
@@ -26,11 +29,53 @@ def sponsors(request):
         .exclude(id__in=last_year_sponsors.values_list("id", flat=True))
     )
 
+    today_year = datetime.date.today().year
+    last_sponsorship_counts = dict(
+        Organization.objects.annotate(
+            last_subsidy_until=Subquery(
+                Subsidy.objects.filter(organization=OuterRef("pk"))
+                .order_by("-date_until")
+                .values("date_until")[:1]
+            ),
+            years_since_last_subsidy=Extract(F("last_subsidy_until"), "year")
+            - today_year,
+            last_sponsorship=Case(
+                When(
+                    Q(years_since_last_subsidy__isnull=True),
+                    then=Value("never"),
+                ),
+                When(
+                    Q(years_since_last_subsidy__lte=0),
+                    then=Value("current"),
+                ),
+                When(
+                    Q(years_since_last_subsidy=1),
+                    then=Value("last_year"),
+                ),
+                When(
+                    Q(years_since_last_subsidy=2),
+                    then=Value("2_years_ago"),
+                ),
+                When(
+                    Q(years_since_last_subsidy__gt=2),
+                    then=Value("more_than_2_years_ago"),
+                ),
+            ),
+        )
+        .values("last_sponsorship")
+        .annotate(nr=Count("last_sponsorship"))
+        .values_list("last_sponsorship", "nr")
+    )
+    last_sponsorship_counts["total"] = sum(
+        count for k, count in last_sponsorship_counts.items() if k != "never"
+    )
+
     context = {
         "current_sponsors": current_sponsors.order_by_yearly_coverage(year, year),
         "last_year_sponsors": last_year_sponsors.order_by_yearly_coverage(
             year - 1, year - 1
         ),
         "past_sponsors": past_sponsors.order_by_yearly_coverage(None, year - 2),
+        "last_sponsorship_counts": last_sponsorship_counts,
     }
     return render(request, "sponsors/sponsors.html", context)
