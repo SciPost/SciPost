172b40478725a691da51ed7ea78c323d99fd09a6
George Katsikas <giorgakis.katsikas@gmail.com>
2024-12-11 17:46:18 +0100

    add few fellow tasks to task list

--- scipost_django/tasks/tasks/task_kinds.py
+++ scipost_django/tasks/tasks/task_kinds.py
@@ -16,7 +16,12 @@ from django.db.models import (
 from django.db.models.functions import Cast, Coalesce
 from django.urls import reverse_lazy
 from django.utils import timezone
-from scipost.templatetags.user_groups import is_financial_admin
+from scipost.templatetags.user_groups import (
+    is_active_fellow,
+    is_ed_admin,
+    is_financial_admin,
+)
+from guardian.shortcuts import get_objects_for_user
 from tasks.tasks.task import TaskKind
 from tasks.tasks.task_action import ViewAction
 
@@ -39,6 +44,11 @@ def get_all_task_kinds(user: "User | None" = None) -> Collection[type[TaskKind]]
         classes = [cls for cls in classes if cls.is_user_eligible(user)]
 
     return classes
+
+
+##########################
+## Financial Admin Tasks
+##########################
 
 
 class ScheduleSubsidyPayments(TaskKind):
@@ -206,4 +216,150 @@ class CheckSubsidyPaymentTask(TaskKind):
                 ),
             )
             .filter(Q(status=SUBSIDY_INVOICED))
+        )
+
+
+#####################
+## Fellow Tasks
+#####################
+
+
+class TreatOngoingAssignmentsTask(TaskKind):
+    name = "Treat Ongoing Assignments"
+    task_title = "{object}"
+    description = "Continue your work for your ongoing editorial assignments."
+    template_name = "tasks/kinds/editorial_assignment.html"
+    actions = [
+        lambda task: ViewAction(
+            url=reverse_lazy(
+                "submissions:editorial_page",
+                kwargs={
+                    "identifier_w_vn_nr": task.data[
+                        "object"
+                    ].submission.preprint.identifier_w_vn_nr
+                },
+            ),
+            content="Editorial page",
+        ),
+    ]
+
+    @staticmethod
+    def is_user_eligible(user):
+        return is_active_fellow(user) if user is not None else False
+
+    @classmethod
+    def get_queryset(cls) -> "QuerySet":
+        from submissions.models.assignment import EditorialAssignment
+
+        if cls.user is None:
+            return EditorialAssignment.objects.none()
+
+        return (
+            EditorialAssignment.objects.ongoing()
+            .filter(to=cls.user.contributor)
+            .prefetch_related("submission")
+        )
+
+
+class VetCommentTask(TaskKind):
+    name = "Vet Comment"
+    task_title = "Vet {object}"
+    description = "Vet a comment that has been submitted for approval."
+    actions = [
+        lambda task: ViewAction(
+            url=reverse_lazy(
+                "comments:vet_submitted_comment",
+                kwargs={"comment_id": task.data["object"].pk},
+            ),
+            content="Vet",
+        ),
+    ]
+
+    @staticmethod
+    def is_user_eligible(user):
+        return (
+            is_active_fellow(user) or is_ed_admin(user) if user is not None else False
+        )
+
+    @classmethod
+    def get_queryset(cls) -> "QuerySet":
+        return get_objects_for_user(
+            cls.user, "comments.can_vet_comments"
+        ).awaiting_vetting()
+
+
+class VetReportTask(TaskKind):
+    name = "Vet Report"
+    task_title = "Vet {object}"
+    description = "Vet a report that has been submitted for approval."
+    actions = [
+        lambda task: ViewAction(
+            url=reverse_lazy(
+                "submissions:vet_submitted_report",
+                kwargs={"report_id": task.data["object"].pk},
+            ),
+            content="Vet",
+        ),
+    ]
+
+    @staticmethod
+    def is_user_eligible(user):
+        return (
+            is_active_fellow(user)
+            or is_ed_admin(user)
+            or user.has_perm("scipost.can_vet_submitted_reports")
+            if user is not None
+            else False
+        )
+
+    @classmethod
+    def get_queryset(cls) -> "QuerySet":
+        from submissions.models.report import Report
+        from submissions.models.submission import Submission
+
+        if cls.user is None:
+            return Report.objects.none()
+
+        if cls.user.has_perm("scipost.can_vet_submitted_reports"):
+            return Report.objects.awaiting_vetting()
+
+        return Report.objects.filter(
+            submission__in=Submission.objects.in_pool_filter_for_eic(
+                cls.user, latest=False, historical=True
+            )
+        ).awaiting_vetting()
+
+
+class SelectRefereeingCycleTask(TaskKind):
+    name = "Select Refereeing Cycle"
+    task_title = "Select Refereeing Cycle for {object}"
+    description = "Select the refereeing cycle for a submission."
+    actions = [
+        lambda task: ViewAction(
+            url=reverse_lazy(
+                "submissions:editorial_page",
+                kwargs={
+                    "identifier_w_vn_nr": task.data[
+                        "object"
+                    ].preprint.identifier_w_vn_nr
+                },
+            ),
+            content="Editorial page",
+        ),
+    ]
+
+    @staticmethod
+    def is_user_eligible(user):
+        return is_active_fellow(user) if user is not None else False
+
+    @classmethod
+    def get_queryset(cls) -> "QuerySet":
+        from submissions.models.submission import Submission
+
+        if cls.user is None:
+            return Submission.objects.none()
+
+        return Submission.objects.filter(
+            editor_in_charge=cls.user.contributor,
+            refereeing_cycle__isnull=True,
         )
