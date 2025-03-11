f13985c58a5ab9b668ec49a21e7f5ffe72a865d3
George Katsikas <giorgakis.katsikas@gmail.com>
2025-03-11 16:34:09 +0100

    feat(api): ✨ add OAuth2 support for DRF
    Add OAuth2 support for DRF. Prettify default OAuth2 templates with crispy forms.

--- scipost_django/SciPost_v1/settings/base.py
+++ scipost_django/SciPost_v1/settings/base.py
@@ -176,7 +176,8 @@ OAUTH2_PROVIDER = {
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.SessionAuthentication"
+        "rest_framework.authentication.SessionAuthentication",
+        "oauth2_provider.contrib.rest_framework.OAuth2Authentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAdminUser",),
     "DEFAULT_FILTER_BACKENDS": [
