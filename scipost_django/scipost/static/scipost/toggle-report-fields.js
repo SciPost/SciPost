1de1117c945b4df3bfbaa17fb5f245ba7e2aabd9
George Katsikas <giorgakis.katsikas@gmail.com>
2025-08-08 14:01:58 +0300

    refactor: ♻️ use css selector for filtering out optional report fields

--- scipost_django/scipost/static/scipost/toggle-report-fields.js
+++ scipost_django/scipost/static/scipost/toggle-report-fields.js
@@ -1,25 +1,15 @@
-$(document).ready(function(){
+let nonRequiredFields = $('form label+:not([required])').parent('div').not('#div_id_use_details').not('#div_id_license_agreement');
+
+$(document).ready(function () {
     $('#showSimpleReportButton').show();
     $('#showFullReportButton').hide();
     $('#reportSelectorButton').on('click', function () {
-	$('#showSimpleReportText').toggle();
-	$('#showSimpleReportButton').toggle();
-	$('#showFullReportText').toggle();
-	$('#showFullReportButton').toggle();
-	$('#id_qualification').parent('div').parent('div').toggle();
-	$('#id_strengths').parent('div').parent('div').toggle();
-	$('#id_weaknesses').parent('div').parent('div').toggle();
-	$('#id_requested_changes').parent('div').parent('div').toggle();
-	$('#id_validity').parent('div').parent('div').toggle();
-	$('#id_significance').parent('div').parent('div').toggle();
-	$('#id_originality').parent('div').parent('div').toggle();
-	$('#id_clarity').parent('div').parent('div').toggle();
-	$('#id_formatting').parent('div').parent('div').toggle();
-	$('#id_grammar').parent('div').parent('div').toggle();
+        nonRequiredFields.toggle();
 
-	$('#previewStrengths').toggle();
-	$('#previewWeaknesses').toggle();
-	$('#previewRequestedChanges').toggle();
-	$('#previewRatings').toggle();
+        $('#previewStrengths').toggle();
+        $('#previewWeaknesses').toggle();
+        $('#previewRequestedChanges').toggle();
+        $('#previewRatings').toggle();
     });
 });
+	
