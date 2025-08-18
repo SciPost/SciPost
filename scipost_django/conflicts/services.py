ae131c4de6d947f323d91d2473dde3828516f580
George Katsikas <giorgakis.katsikas@gmail.com>
2025-08-18 16:54:19 +0300

    refactor: ♻️ add resource date to conflict creation method

--- scipost_django/conflicts/services.py
+++ scipost_django/conflicts/services.py
@@ -3,6 +3,8 @@ __license__ = "AGPL v3"
 
 
 # Module for making external api calls as needed in the submissions cycle
+from datetime import datetime
+from typing import Any
 import feedparser
 import logging
 
@@ -31,6 +33,24 @@ class ArxivCaller:
             return "title" in data["entries"][0]
         return False
 
+    @staticmethod
+    def create_conflict(
+        profile, rel_profile, entry: dict[str, Any]
+    ) -> tuple[ConflictOfInterest, bool]:
+        uploaded_date = entry.get("updated", None)
+        if isinstance(uploaded_date, str):
+            uploaded_date = datetime.fromisoformat(uploaded_date)
+
+        coi, new = ConflictOfInterest.objects.get_or_create(
+            header=entry["title"],
+            url=entry["link"].replace("http:", "https:"),
+            profile=profile,
+            related_profile=rel_profile,
+            type="coauthor",
+            resource_date=uploaded_date,
+        )
+        return coi, new
+
     def compare(self, author_profiles, relating_profiles, submission=None):
         """
         Compare two list of Profiles using the arXiv API.
@@ -56,14 +76,8 @@ class ArxivCaller:
                 )
 
                 if self._search_result_present(response_content):
-                    for conflict in response_content["entries"]:
-                        coi, new = ConflictOfInterest.objects.get_or_create(
-                            header=conflict["title"],
-                            url=conflict["link"].replace("http:", "https:"),
-                            profile=profile,
-                            related_profile=rel_profile,
-                            type="coauthor",
-                        )
+                    for entry in response_content["entries"]:
+                        coi, new = self.create_conflict(profile, rel_profile, entry)
                         if submission:
                             coi.related_submissions.add(submission)
                         count += 1 if new else 0
