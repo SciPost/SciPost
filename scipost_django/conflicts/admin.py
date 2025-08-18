ab33e98f7b6f1719cea5be0272293b94fa0a906f
George Katsikas <giorgakis.katsikas@gmail.com>
2025-08-18 16:55:13 +0300

    feat: 🚸 add full name search for CoI admin

--- scipost_django/conflicts/admin.py
+++ scipost_django/conflicts/admin.py
@@ -11,8 +11,8 @@ from .models import ConflictOfInterest
 class ConflictAdmin(admin.ModelAdmin):
     search_fields = (
         "header",
-        "profile__last_name",
-        "related_profile__last_name",
+        "profile__full_name",
+        "related_profile__full_name",
         "related_submissions__title",
         "related_submissions__preprint__identifier_w_vn_nr",
     )
