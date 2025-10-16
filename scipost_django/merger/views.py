015cb3da45784bf4584a7f1344347904991b391c
George Katsikas <giorgakis.katsikas@gmail.com>
2025-10-16 12:23:18 +0300

    feat: :sparkles: add merge util and views

--- scipost_django/merger/views.py
+++ scipost_django/merger/views.py
@@ -1,6 +1,7 @@
 __copyright__ = "Copyright © Stichting SciPost (SciPost Foundation)"
 __license__ = "AGPL v3"
 
+from django.contrib import messages
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import BadRequest
@@ -10,10 +11,18 @@ from django.http import Http404, HttpRequest, HttpResponse
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 
-from .models import NonDuplicateMark
-from .utils import M, FieldOrRel, FieldValues, resolve_field_value
-
 from scipost.permissions import HTMXPermissionsDenied, HTMXResponse
+
+from .models import NonDuplicateMark, MergeHistoryRecord
+from .utils import (
+    M,
+    FieldOrRel,
+    FieldValues,
+    resolve_field_value,
+    merge_objects,
+    MergeChangeType,
+    MergeStrategy,
+)
 
 from typing import Any
 
@@ -153,6 +162,9 @@ class BaseComparisonView(PermissionRequiredMixin, TemplateView):
 
         resolved_fields = {}
         for field in model_fields:
+            field.merge_strategies = MergeStrategy.get_admissible_strategies(field)
+            field.selected_strategy = MergeStrategy.default_for_field(field)
+
             # We don't care about GeneratedFields or "calculated fields" (cf_)
             if isinstance(field, GeneratedField) or field.name.startswith("cf_"):
                 continue
@@ -383,3 +395,280 @@ class HXNonDuplicateMarkCreateView(BaseComparisonView):
             "Objects marked as non-duplicates.",
             tag="success",
         )
+
+
+class HXMergeView(BaseComparisonView):
+    """
+    If GET: Display a "diff" view of the two objects, with buttons to merge them.
+    If POST: Merge the two objects, keeping a record of the merge in the database.
+    """
+
+    template_name = "merger/merge.html"
+    permission_required = "scipost.can_merge_objects"
+
+    @staticmethod
+    def compute_field_merge_changes(
+        field: FieldOrRel,
+        from_vals: FieldValues,
+        to_vals: FieldValues,
+        strategy: MergeStrategy | None = None,
+    ) -> list[tuple[MergeChangeType, Model | None]]:
+        """
+        Produces a preview of what the merged field will look like.
+        Return a list of tuples, where the first element is the status of the object (unchanged, added, removed)
+        """
+        strategy = strategy or MergeStrategy.default_for_field(
+            field, from_vals, to_vals
+        )
+
+        changes: list[tuple[MergeChangeType, Model | None]] = []
+
+        if (
+            (not field.is_relation or field.one_to_one or field.many_to_one)
+            and (from_val := next(iter(from_vals), None))
+            and (to_val := next(iter(to_vals), None))
+        ):
+            match strategy.to_tuple():
+                case (MergeStrategy.FieldRetainment.KEEP, deprecation_strategy):
+                    merge_change_type = (
+                        deprecation_strategy.merge_change_type
+                        if deprecation_strategy
+                        else MergeChangeType.REMOVED
+                    )
+                    if from_val == to_val or not from_val:  # Empty "from" or identical
+                        changes = [(MergeChangeType.UNCHANGED, to_val)]
+                    elif not to_val:  # Empty "to" value, so "from" value prevails
+                        changes = [(MergeChangeType.ADDED, from_val)]
+                    elif from_val:  # Non-empty "from" removed and "to" unchanged
+                        changes = [
+                            (merge_change_type, from_val),
+                            (MergeChangeType.UNCHANGED, to_val),
+                        ]
+                case (MergeStrategy.FieldRetainment.REPLACE, deprecation_strategy):
+                    merge_change_type = (
+                        deprecation_strategy.merge_change_type
+                        if deprecation_strategy
+                        else MergeChangeType.REMOVED
+                    )
+                    if from_val == to_val:  # Same value, just appear unchanged
+                        changes = [(MergeChangeType.UNCHANGED, to_val)]
+                    elif not from_val:  # Empty "from" value, so "to" value prevails
+                        changes = [(MergeChangeType.UNCHANGED, to_val)]
+                    elif to_val:  # Overwrite "to" value with "from" value
+                        changes = [
+                            (merge_change_type, to_val),
+                            (MergeChangeType.ADDED, from_val),
+                        ]
+                case _:
+                    raise NotImplementedError(
+                        f"Merge strategy {strategy} not implemented for single-valued fields."
+                    )
+        elif field.many_to_many or field.one_to_many:
+            retainment, deprecation = strategy.to_tuple()
+            if (
+                retainment != MergeStrategy.FieldRetainment.COMBINE
+                and deprecation is None
+            ):
+                raise ValueError(
+                    "Deprecation strategy must be provided for multi-valued fields."
+                )
+
+            match retainment:
+                case MergeStrategy.FieldRetainment.KEEP:
+                    existing = [(MergeChangeType.UNCHANGED, o) for o in to_vals if o]
+                    removed = [
+                        (deprecation.merge_change_type, o)
+                        for o in set(from_vals) - set(to_vals)
+                        if o
+                    ]
+                    changes = existing + removed
+                case MergeStrategy.FieldRetainment.REPLACE:
+                    added = [
+                        (MergeChangeType.ADDED, o)
+                        for o in set(from_vals) - set(to_vals)
+                        if o
+                    ]
+                    existing = [
+                        (MergeChangeType.UNCHANGED, o)
+                        for o in to_vals
+                        if o and o in from_vals
+                    ]
+                    removed = [
+                        (deprecation.merge_change_type, o)
+                        for o in set(to_vals) - set(from_vals)
+                        if o
+                    ]
+                    changes = added + removed + existing
+                case MergeStrategy.FieldRetainment.COMBINE:
+                    existing = [(MergeChangeType.UNCHANGED, o) for o in to_vals if o]
+                    added = [
+                        (MergeChangeType.ADDED, o)
+                        for o in set(from_vals) - set(to_vals)
+                        if o
+                    ]
+                    changes = existing + added
+                case _:
+                    raise NotImplementedError(
+                        f"Merge strategy {strategy} not implemented for multi-valued fields."
+                    )
+
+        return sorted(changes, key=lambda c: c[0].name, reverse=True)
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+
+        content_type = self.get_content_type()
+        object_from, object_to = self.get_objects()
+        non_duplicate_declaration = self.get_non_duplicate_declaration(
+            object_from, object_to
+        )
+
+        context |= {
+            "content_type": content_type,
+            "objects": [object_from, object_to],
+            "object_from": object_from,
+            "object_to": object_to,
+            "non_duplicate_declaration": non_duplicate_declaration,
+        }
+
+        if model := content_type.model_class():
+            obj_a_field_data = self.get_object_field_data(object_from)
+            obj_b_field_data = self.get_object_field_data(object_to)
+
+            object_field_changes = {
+                field: (field_name, changes)
+                for (field, (field_name, from_val)), (_, (_, to_val)) in zip(
+                    obj_a_field_data.items(), obj_b_field_data.items()
+                )
+                if (
+                    changes := self.compute_field_merge_changes(field, from_val, to_val)
+                )
+            }
+
+            context |= {
+                "model_name": model._meta.verbose_name,
+                "model_name_plural": model._meta.verbose_name_plural,
+                "object_field_changes": object_field_changes,
+            }
+
+        return context
+
+    def post(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        content_type = self.get_content_type()
+        model_class = content_type.model_class()
+        object_from, object_to = self.get_objects()
+
+        # request.POST will have a `field_name` array to which we will match the strategies
+        field_strategies: dict[FieldOrRel, MergeStrategy] = {
+            field: MergeStrategy(strategy)
+            for name in request.POST.getlist("field_name")
+            if (strategy := request.POST.get(f"field_{name}_strategy"))
+            and model_class is not None
+            and (field := model_class._meta.get_field(name))
+        }
+
+        try:
+            merge_history_record = MergeHistoryRecord.from_operation(
+                performed_by=request.user.contributor,
+                deprecated=object_from,
+                kept=object_to,
+                options={
+                    "strategies": {
+                        field.name: [
+                            strategy_part.value if strategy_part is not None else None
+                            for strategy_part in strategy.to_tuple()
+                        ]
+                        for field, strategy in field_strategies.items()
+                    }
+                },
+                description=request.headers.get("HX-Prompt", ""),
+            )
+
+            merge_objects(object_from, object_to, field_strategies)
+
+            merge_history_record.save()
+
+            # Delete the "from" object unless it favors being kept
+            # and "rerouted" via a "duplicate_of" field.
+            if hasattr(object_from, "duplicate_of"):
+                setattr(object_from, "duplicate_of", object_to)
+                object_from.save()
+            else:
+                object_from.delete()
+
+        except Exception as e:
+            messages.error(request, f"Error during merge: {e}")
+            redirect_url = request.get_full_path()
+        else:
+            messages.success(request, "Objects merged successfully.")
+            redirect_url = reverse_lazy("merger:duplicates", args=[content_type.model])
+
+        response = HttpResponse()
+        response.headers["HX-Redirect"] = redirect_url
+        return response
+
+
+class HXMergePreviewFieldView(HXMergeView):
+    """
+    View for previewing the merge of a single field.
+    """
+
+    permission_required = "scipost.can_compare_objects"
+    template_name = "merger/_hx_merge_field.html"
+
+    def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        if request.headers.get("HX-Request") != "true":
+            raise BadRequest("This view is only accessible via HTMX.")
+
+        if not request.user.has_perm("scipost.can_compare_objects"):
+            return HTMXPermissionsDenied(
+                "You do not have permission to compare objects."
+            )
+
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+
+        if not (field_name := self._get_param("field_name")):
+            raise BadRequest("No field name provided.")
+
+        if not (
+            (strategy_str := self._get_param(f"field_{field_name}_strategy"))
+            and (selected_strategy := MergeStrategy(strategy_str))
+        ):
+            raise BadRequest("No merge strategy provided.")
+
+        content_type = self.get_content_type()
+
+        try:
+            field = content_type.model_class()._meta.get_field(field_name)
+        except Exception as e:
+            raise AttributeError(f"Error retrieving field: {e}")
+
+        field_merge_strategies = MergeStrategy.get_admissible_strategies(field)
+        if selected_strategy not in field_merge_strategies:
+            raise ValueError("Invalid merge strategy provided for this field.")
+
+        object_from, object_to = self.get_objects()
+
+        try:
+            from_val = self.get_object_field_data(object_from)[field][1]
+            to_val = self.get_object_field_data(object_to)[field][1]
+        except KeyError:
+            raise KeyError("Field not found on one of the objects.")
+
+        changes = self.compute_field_merge_changes(
+            field, from_val, to_val, selected_strategy
+        )
+
+        field.selected_strategy = selected_strategy
+
+        context |= {
+            "field": field,
+            "from_val": from_val,
+            "to_val": to_val,
+            "changes": changes,
+        }
+
+        return context
