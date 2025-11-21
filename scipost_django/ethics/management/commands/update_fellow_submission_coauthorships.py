d954e34fc000230f5b5f3c3748eae2ed481ae51e
George Katsikas <giorgakis.katsikas@gmail.com>
2025-11-21 13:14:00 +0200

    fix: 🐛 subs without author matching stuck fetching coauthorships

--- scipost_django/ethics/management/commands/update_fellow_submission_coauthorships.py
+++ scipost_django/ethics/management/commands/update_fellow_submission_coauthorships.py
@@ -11,14 +11,22 @@ from submissions.models import Submission
 class Command(BaseCommand):
     """Compare submission fellows and authors for potential coauthorships."""
 
+    SUBMISSIONS_PROCESSED_PER_RUN = 1
+
     def handle(self, *args, **options):
         submissions: list[Submission] = list(
             Submission.objects.all()
+            .stage_incoming_completed()
             .needs_coauthorships_update()
-            .prefetch_related("fellows")[:1]
+            .prefetch_related("fellows")
+            .order_by("latest_activity")
         )
 
+        submissions_processed = 0
         for submission in submissions:
+            if submissions_processed >= self.SUBMISSIONS_PROCESSED_PER_RUN:
+                break
+
             if not submission.enough_author_profiles_matched:
                 continue
 
@@ -39,3 +47,5 @@ class Command(BaseCommand):
 
             submission.needs_coauthorships_update = False
             submission.save()
+
+            submissions_processed += 1
