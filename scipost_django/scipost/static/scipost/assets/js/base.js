6fb98ae4ff6a41ba533818182e1ebdf1f4b74907
George Katsikas <giorgakis.katsikas@gmail.com>
2026-02-26 13:31:01 +0200

    fix: 🧑‍💻 make htmx package available globally

--- scipost_django/scipost/static/scipost/assets/js/base.js
+++ scipost_django/scipost/static/scipost/assets/js/base.js
@@ -6,6 +6,7 @@ import 'bootstrap'
 
 // import 'htmx.org'
 var htmx = require('htmx.org');
+window.htmx = htmx; // Make htmx available globally
 
 // To prevent all tabs from being updated if acad_field or specialty are changed,
 // tweak the HTMX request by letting through only the one(s) of the active portal tab:
