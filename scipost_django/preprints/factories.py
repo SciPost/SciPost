754496b27dee520accadd3451e2da3b834848c42
George Katsikas <giorgakis.katsikas@gmail.com>
2026-06-09 16:04:31 +0300

    feat: ✨ add version option to preprint factory

--- scipost_django/preprints/factories.py
+++ scipost_django/preprints/factories.py
@@ -2,12 +2,16 @@ __copyright__ = "Copyright © Stichting SciPost (SciPost Foundation)"
 __license__ = "AGPL v3"
 
 
+import re
+
 import factory
 
 from common.helpers import random_arxiv_identifier_with_version_number
+from submissions.models.submission import Submission
 
 from .models import Preprint
 
+IDENTIFIER_VERSION_NUMBER_REGEX = r"v(\d{1,2})$"
 
 class PreprintFactory(factory.django.DjangoModelFactory):
     identifier_w_vn_nr = factory.Faker("numerify", text="test_####.####")
@@ -24,6 +28,36 @@ class PreprintFactory(factory.django.DjangoModelFactory):
             ),
         )
         scipost = factory.Trait(
-            identifier_w_vn_nr=factory.Faker("numerify", text="scipost_######_#####v#"),
+            identifier_w_vn_nr=factory.Faker("numerify", text="scipost_######_#####"),
             _file=factory.django.FileField(filename="submission.pdf"),
         )
+
+    @factory.post_generation
+    def version_number(self, create, extracted, **kwargs):
+        # Skip if the identifier already has a version number (e.g., arXiv-based ones)
+        if re.search(IDENTIFIER_VERSION_NUMBER_REGEX, self.identifier_w_vn_nr):
+            return
+
+        if not create:
+            return
+        if extracted:
+            if not isinstance(extracted, int):
+                raise ValueError("Version number must be an integer.")
+            if not (1 <= extracted <= 99):
+                raise ValueError("Version number must be between 1 and 99.")
+
+            version = extracted
+        else:
+            try:
+                latest_submission = self.submission.get_latest_version()
+
+                if latest_version_no := re.search(
+                    IDENTIFIER_VERSION_NUMBER_REGEX,
+                    latest_submission.preprint.identifier_w_vn_nr,
+                ):
+                    version = int(latest_version_no.group(1)) + 1
+            except Submission.DoesNotExist:
+                version = 1
+
+        self.identifier_w_vn_nr += f"v{version}"
+        self.save()
